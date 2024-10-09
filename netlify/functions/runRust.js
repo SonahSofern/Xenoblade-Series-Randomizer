bbd903c610b4f50821aeae6166f16745774c06d2
SonahSofern <94713741+BenjaminSnarky@users.noreply.github.com>
2024-10-08 20:09:55 -0900

    Server code to run rust

---
+++ netlify/functions/runRust.js
@@ -0,0 +1,21 @@
+// netlify/functions/runRust.js
+const { exec } = require('child_process');
+
+exports.handler = async function(event, context) {
+  return new Promise((resolve, reject) => {
+    exec('/XC2_Randomizer/bdat-toolset-win64.exe', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing Rust binary: ${error}`);
+        return reject({
+          statusCode: 500,
+          body: 'Server Error'
+        });
+      }
+
+      resolve({
+        statusCode: 200,
+        body: stdout,
+      });
+    });
+  });
+};
