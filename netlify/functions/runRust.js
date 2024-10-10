06bd05b4760e46b2b9288b342b83b160fdb1b8ba
SonahSofern <94713741+BenjaminSnarky@users.noreply.github.com>
2024-10-10 14:00:24 -0900

    Update runRust.js

--- netlify/functions/runRust.js
+++ netlify/functions/runRust.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exports.handler = async function (event, context) {
+exports.handler = async (event, context) => {
     // let commonbdat = document.getElementById("commonbdat").value; // node js cant access this 
     // exec(`./linuxBDAT info ${commonbdat} -t TableName'`)
     console.log("Just see what happens");
