f145bbc38a8ff7ef4b763649ea91eacc10af68f6
SonahSofern <94713741+BenjaminSnarky@users.noreply.github.com>
2024-10-10 13:51:19 -0900

    SAsa

--- Website/JS/XC2_Rando.js
+++ Website/JS/XC2_Rando.js
@@ -1,3 +1,10 @@
 function updatePercentage(slider){
     slider.nextElementSibling.innerText = slider.value + '%';
+//     let test = document.getElementById("commonbdat");
+//     if (test != null){
+//         console.log(test.value);
+//     }
+//     else{
+// console.log("file not here");
+   // }
 }
--- netlify/functions/runRust.js
+++ netlify/functions/runRust.js
@@ -1,25 +1,7 @@
-// netlify/functions/runRust.js
 const { exec } = require('child_process');
-const path = require('path'); 
 
 exports.handler = async function (event, context) {
-  return new Promise((resolve, reject) => {
-    // Call the Rust binary with arguments
-    exec('./linuxBDAT info file.bdat -t TableName', (error, stdout, stderr) => {
-      if (error) {
-        console.error('Error executing Rust binary:', error);
-        reject({
-          statusCode: 500,
-          body: 'Error executing Rust binary',
-        });
-        return;
-      }
-
-      console.log('stdout:', stdout);
-      resolve({
-        statusCode: 200,
-        body: stdout || stderr,
-      });
-    });
-  });
-};
+    // let commonbdat = document.getElementById("commonbdat").value; // node js cant access this 
+    // exec(`./linuxBDAT info ${commonbdat} -t TableName'`)
+    console.log("Just see what happens");
+  }
