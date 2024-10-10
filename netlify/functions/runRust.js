06f9e917207894a64aaa7137a174763440974899
SonahSofern <94713741+BenjaminSnarky@users.noreply.github.com>
2024-10-09 18:23:53 -0900

    Update runRust.js

--- netlify/functions/runRust.js
+++ netlify/functions/runRust.js
@@ -5,6 +5,9 @@ const path = require('path');
 exports.handler = async function(event, context) {
     const linuxBdatPath = path.join(__dirname, './linuxBDAT');
     console.log(linuxBdatPath);
+    var fs = require('fs');
+    var files = fs.readdirSync("/var/task");
+    console.log(files);
     console.log('Current working directory:', __dirname);
   return new Promise((resolve, reject) => {
     exec(linuxBdatPath, (error, stdout, stderr) => {
