d4b916f1fdff33a8bfa586d42c7221e9ac9c6620
SonahSofern <94713741+BenjaminSnarky@users.noreply.github.com>
2024-10-09 19:31:54 -0900

    Update runRust.js

--- netlify/functions/runRust.js
+++ netlify/functions/runRust.js
@@ -2,26 +2,23 @@
 const { exec } = require('child_process');
 const path = require('path'); 
 
-exports.handler = async function(event, context) {
-    const linuxBdatPath = path.join(__dirname, '/linuxBDAT');
-    console.log(linuxBdatPath);
-    var fs = require('fs');
-    var files = fs.readdirSync("/var");
-    console.log(files);
-    console.log('Current working directory:', __dirname);
+exports.handler = async function (event, context) {
   return new Promise((resolve, reject) => {
-    exec("xenoblade2randomizer/netlify/functions/linuxbdat", (error, stdout, stderr) => {
+    // Call the Rust binary with arguments
+    exec('./bdat-toolset info file.bdat -t TableName', (error, stdout, stderr) => {
       if (error) {
-        console.error(`Error executing Rust binary: ${error}`);
-        return reject({
+        console.error('Error executing Rust binary:', error);
+        reject({
           statusCode: 500,
-          body: 'Server Error'
+          body: 'Error executing Rust binary',
         });
+        return;
       }
 
+      console.log('stdout:', stdout);
       resolve({
         statusCode: 200,
-        body: stdout,
+        body: stdout || stderr,
       });
     });
   });
