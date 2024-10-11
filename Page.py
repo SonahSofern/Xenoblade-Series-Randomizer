c75660032e08c0e68644711f47762dc073d8fc13
SonahSofern <94713741+BenjaminSnarky@users.noreply.github.com>
2024-10-10 17:46:44 -0900

    Fixing code

--- Page.py
+++ Page.py
@@ -3,26 +3,14 @@ from tkinter import PhotoImage
 import os
 import sys
 
-# create root window
+
 root = tk.Tk()
 
-# root window title and dimension
 root.title("Xenoblade Chronicles 2 Randomizer")
-# Set geometry (widthxheight)
+
 root.geometry('1000x600')
 
-# Function to get the correct path for resources
-def resource_path(relative_path):
-    if getattr(sys, 'frozen', False):
-        base_path = sys._MEIPASS  # Temporary folder for bundled files
-    else:
-        base_path = os.path.dirname(__file__)  # Directory of the script
-
-    return os.path.join(base_path, relative_path)
-
-# Load the icon using the resource path function
-icon_path = resource_path('Images\XC2Icon.png')
-icon = PhotoImage(file=icon_path)
+icon = PhotoImage(file = os.path.join(sys._MEIPASS, 'Images\XC2Icon.png'))
 
 root.iconphoto(False, icon)
 
