fb953c6233a6ffa213b6ea36cfd64e351dd4a86d
SonahSofern <94713741+BenjaminSnarky@users.noreply.github.com>
2024-10-15 14:28:46 -0900

    Clearer Logic

--- JSONParser.py
+++ JSONParser.py
@@ -6,13 +6,14 @@ def RandomizeBetweenRange(cmdDescription, Filename, keyWord, rangeValuesToReplac
         # print(cmdDescription + " Slider at zero") 
         return
     print(cmdDescription)   
-    with open("./_internal/JsonOutputs/common/" + Filename, 'r+', encoding='utf-8') as file:
-        data = json.load(file)
-        for row in data['rows']:
-            for key, value in row.items():
-                if key.startswith(keyWord):
-                    if ((random.randint(0,100) <= sliderOdds) and ((row[key] in rangeValuesToReplace))):
-                        row[key] = random.choice(rangeValidReplacements)
-        file.seek(0)
-        file.truncate()
-        json.dump(data, file, indent=2)
+    for name in Filename:
+      with open("./_internal/JsonOutputs/common/" + name, 'r+', encoding='utf-8') as file:
+          data = json.load(file)
+          for row in data['rows']:
+              for key, value in row.items():
+                  if any((key == k) for k in keyWord):
+                      if ((random.randint(0,100) <= sliderOdds) and ((row[key] in rangeValuesToReplace))):
+                          row[key] = random.choice(rangeValidReplacements)
+          file.seek(0)
+          file.truncate()
+          json.dump(data, file, indent=2)
