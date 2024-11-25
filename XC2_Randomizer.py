ad8b5b85e697951ece701d986968b18e55b9f2ca
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2024-11-24 18:22:47 -0900

    Update XC2_Randomizer.py

--- XC2_Randomizer.py
+++ XC2_Randomizer.py
@@ -246,13 +246,13 @@ def Randomize():
         subprocess.run(f"./_internal/Toolset/bdat-toolset-win64.exe extract {bdatFilePathEntry.get()}/gb/common_ms.bdat -o {JsonOutput} -f json --pretty")
 
         for name, option in OptionsRunDict.items():
-            randoDisplay.pack()
+            randoProgressDisplay.pack()
             if (option["optionType"].get()):
-                randoDisplay.config(text=f"Randomizing {name}")
+                randoProgressDisplay.config(text=f"Randomizing {name}")
                 for command in option["commandList"]:
                     command()
 
-        randoDisplay.pack_forget()
+        randoProgressDisplay.pack_forget()
 
         subprocess.run(f"./_internal/Toolset/bdat-toolset-win64.exe pack {JsonOutput} -o {outDirEntry.get()} -f json")
         os.makedirs(f"{outDirEntry.get()}/gb", exist_ok=True)
@@ -295,7 +295,7 @@ RandomizeParent = Frame(root, background=Red)
 RandomizeParent.pack()
 RandomizeButton = Button(RandomizeParent,text='Randomize', command=Randomize)
 RandomizeButton.pack(pady=10, side='left') 
-randoDisplay = Label(text="", background=Red, anchor="e", foreground=White)
+randoProgressDisplay = Label(text="", background=Red, anchor="e", foreground=White)
 
 EveryObjectToSave = ([bdatFilePathEntry, outDirEntry, randoSeedEntry] + CheckboxStates + OptionInputs)
 SavedOptions.loadData(EveryObjectToSave)
