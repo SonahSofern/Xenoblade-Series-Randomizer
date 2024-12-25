04d9382869557e97d384b019e7f54ee94b6209a2
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2024-12-24 15:20:21 -0900

    Permalink workin

--- scripts/SavedOptions.py
+++ scripts/SavedOptions.py
@@ -16,12 +16,6 @@ def loadData(DataList, Filename):
             file.seek(0)
             savedLines = file.readlines()
             for i in range(len(savedLines)):
-                if isinstance(DataList[i], ttk.Entry):
-                    DataList[i].delete(0,tk.END)
-                    DataList[i].insert(0, savedLines[i].strip())
-                elif ((isinstance(DataList[i], tk.BooleanVar)) or (isinstance(DataList[i], tk.IntVar))):
-                    DataList[i].set(savedLines[i].strip())
-                else:
-                    DataList[i]= savedLines[i].strip()
+                DataList[i].set(savedLines[i].strip())
     except:
         print("Error Loading Settings Saved Values (Likely an option was added or removed)")
