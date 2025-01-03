7203def67cd424d5d921b4a7e27b90517ab3fce5
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2025-01-02 15:31:06 -0900

    Good for now

--- scripts/SkillTrees.py
+++ scripts/SkillTrees.py
@@ -5,9 +5,9 @@ from Enhancements import *
 def RandomizeSkillEnhancements():
     ArtsCancelSlots = [12,55,82,102,131,161]
     ZekeEye = [152]
-    X_Slots= [1,56,83,103,132,162]
-    Y_Slots= [11,57,84,104,133,163]
-    B_Slots= [21,58,85,105,134,164] 
+    X_Slots= [1,51,61,91,121,162]
+    Y_Slots= [11,31,71,101,141,171]
+    B_Slots= [21,52,62,92,122,151] 
     InvalidSkillEnhancements = [ArtCancel, EyeOfJustice, XStartBattle, YStartBattle, BStartBattle]
     ValidSkills = [x for x in EnhanceClassList if x not in InvalidSkillEnhancements]
     ForcedSkills = []
@@ -39,21 +39,3 @@ def RandomizeSkillEnhancements():
         enhancementFile.seek(0)
         enhancementFile.truncate()
         json.dump(enhanceFile, enhancementFile, indent=2, ensure_ascii=False)
-    # ShuffleSkills()
-
-# def ShuffleSkills():
-#     NumDrivers = 6
-#     for i in range(NumDrivers):
-#         S1 = [2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-#         S2 = [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-#         random.shuffle(S1)
-#         random.shuffle(S2)
-#         with open(f"./_internal/JsonOutputs/common/BTL_Skill_Dr_Table0{i+1}.json", 'r+', encoding='utf-8') as driverSkillFile:
-#             skill = json.load(driverSkillFile)
-#             for j in range(2,16):
-#                 skill["rows"][j-1]["$id"] = S1[j-2]
-#             for k in range(16,31):
-#                 skill["rows"][k-16]["$id"] = S2[k-17]
-#             driverSkillFile.seek(0)
-#             driverSkillFile.truncate()
-#             json.dump(skill, driverSkillFile, indent=2, ensure_ascii=False)
