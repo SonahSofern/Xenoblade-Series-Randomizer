23af9281e21fe92394c3866f73c8770de9024d8c
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2025-01-06 15:47:03 -0900

    Update _EnemyStats.py

--- scripts/_EnemyStats.py
+++ scripts/_EnemyStats.py
@@ -9,20 +9,37 @@ class Attribute:
     nameColor = ""
     enPar = ""
     enArr = ""
-    def __init__(self,_name, _baseMult, _nameColor, _enPar = "", _enArr = ""):
+    upgradable = True
+    ignoreOriginal = False
+    def __init__(self,_name, _baseMult ,_enPar = "", _nameColor = "tutorial", isUp = True, ignoreOriginal = False):
         self.name = _name
         self.mult = _baseMult
         self.nameColor = _nameColor
         self.enPar = _enPar
-        self.enArr = _enArr
+        self.isUp = isUp
+        self.ignoreOriginal = ignoreOriginal
         AttributesList.append(self)
     def RollRank(self):
-        rank = random.randrange(0,3)
+        rank = random.randrange(1,4)
         return rank
 
 AttributesList = []
+# working colors green, red, tutorial(yellow), link(blue)
 
-Golden = Attribute("Golden" ,5, "tutorial", _enArr = "GoldRev")
+Speedy = Attribute("Speed", 0.9, "BtlSpeed")
+Health = Attribute("Health", 0.4, "HpMaxRev")
+Strength = Attribute("Strength", 0.25, "StrengthRev")
+Ether = Attribute("Ether", 0.3, "PowEtherRev")
+Dex = Attribute("Dex", 0.5, "DexRev")
+Agility = Attribute("Agility", 0.3, "AgilityRev")
+Luck = Attribute("Luck", 0.5, "LuckRev")
+PArmor = Attribute("Defense", 0.2, "RstPower")
+EArmor =Attribute("E. Defense", 0.3, "RstEther")
+RstFire = Attribute("Fireproof", 0.3, "RstFire")
+Crit = Attribute("Critical", 0.3, "CriticalRate")
+GuardRate = Attribute("Guard", 0.3, "GuardRate")
+Unpottable = Attribute("Potion", 0, "HealPotDrop")
+
 
 
 def EnemyStats(spinBox):
@@ -36,20 +53,21 @@ def EnemyStats(spinBox):
                     if spinBox >= random.randrange(0,101):
                         Att = random.choice(AttributesList)
                         rank = Att.RollRank()
-                        
-                        if Att.enArr != "":
-                            Enemy[Att.enArr] *= (Att.mult * rank+1) # Changes EnArrangeFile
+                    
                         
                         if Att.enPar != "":
                             for par in EnPar["rows"]: # Changes EnParamFile
                                 if par["$id"] == Enemy["ParamID"]:
-                                    par[Att.enPar] *= (Att.mult * rank+1)
-                                    break
+                                    if par[Att.enPar] == 0: # For things like RstElement
+                                       par[Att.enPar] += (Att.mult * rank)
+                                    else:         
+                                        par[Att.enPar] *= (Att.mult + rank)
+                                        break
                                 
                         for name in Names["rows"]: # Changes Names
                             if name["$id"] == Enemy["Name"]:
                                 oldName = name["name"]
-                                name["name"] = f"[System:Color name={Att.nameColor}]{Att.name +  ("+"*rank)}[/System:Color] {oldName}"
+                                name["name"] = f"[System:Color name={Att.nameColor}]{Att.name +  ('+'*rank)}[/System:Color] {oldName}"
                                 break
 
                 
