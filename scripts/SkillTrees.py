6ee2b743439786a648e8ac6a52b1c5711dfadf85
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2025-01-08 20:23:38 -0900

    Skill trees fixed

--- scripts/SkillTrees.py
+++ scripts/SkillTrees.py
@@ -5,45 +5,61 @@ from Enhancements import *
 
 def RandomizeSkillEnhancements(optDict):
     ArtsCancelSlots = [12,55,82,102,131,161]
-    ZekeEye = [152]
+    ZekeEyeSlot = [152]
     X_Slots= [1,51,61,91,121,162]
     Y_Slots= [11,31,71,101,141,171]
-    B_Slots= [21,52,62,92,122,151] 
+    B_Slots= [21,52,62,92,122,151]
+    AllSpecialSlots = ArtsCancelSlots + ZekeEyeSlot + X_Slots + Y_Slots + B_Slots
     isVanilla = not optDict["Driver Skill Trees"]["subOptionObjects"]["Nonstandard Skills"]["subOptionTypeVal"].get()
     with open("./_internal/JsonOutputs/common/BTL_Skill_Dr.json", 'r+', encoding='utf-8') as enhancementFile:
         with open("./_internal/JsonOutputs/common_ms/btl_skill_dr_name.json", 'r+', encoding='utf-8') as nameFile:
             enhanceFile = json.load(enhancementFile)
             skillNameFile = json.load(nameFile)
-            
+                                    # ArtCancelEnhanceIDs.remove(newOne[1])
             if isVanilla:
-                def FindSkill(skillList,targetIDs):
+                def FindSkill(skillList,targetEnhancementIDs):
                     for skill in skillList:
-                        if skill["Enhance"] in targetIDs:
+                        if skill[1] in targetEnhancementIDs:
+                            targetEnhancementIDs.remove(skill[1])
+                            skillList.remove(skill)
                             return skill
                 ArtCancelEnhanceIDs = [1280, 187, 1309,1340,1368,1397,1431,1450,1464,1494,1523,2246]
                 X_StartIDs = [1277,1327,1321,1369,1389, 1427,1447, 1461,1491,1513]
                 Y_StartIDs = [1290,1308,1349,1360,1398,1419,1448,1474,1483,1521]
                 B_StartIDs = [110,1268,1299,1338,1378,1411,1440,1449,1452,1504,1534]
                 ZekeEyeIDs = [1444]
+                AllSpecialEnhances = ArtCancelEnhanceIDs + X_StartIDs + Y_StartIDs + B_StartIDs + ZekeEyeIDs
                 skillList = []
+                specialSkillList = []
                 for skillSlot in enhanceFile['rows']: # Make a list of ids
-                    skillList.append([skillSlot["Name"],skillSlot["Enhance"]])
+                    if skillSlot["Enhance"] in AllSpecialEnhances:
+                        specialSkillList.append([skillSlot["Name"],skillSlot["Enhance"]])
+                    else:
+                        skillList.append([skillSlot["Name"],skillSlot["Enhance"]])
+                        
+                random.shuffle(skillList)
+                random.shuffle(specialSkillList)
+
                     
-                for skillSlot in enhanceFile["rows"]: # Shuffle Ids
+                for skillSlot in enhanceFile["rows"]:  # Set special Cases
                     id = skillSlot["$id"]
                     if id in ArtsCancelSlots:
-                        newOne = FindSkill(skillList,ArtCancelEnhanceIDs)
+                        newOne = FindSkill(specialSkillList,ArtCancelEnhanceIDs)
                     elif id in X_Slots:
-                        newOne = FindSkill(skillList,X_StartIDs)
+                        newOne = FindSkill(specialSkillList,X_StartIDs)
                     elif id in Y_Slots:
-                        newOne = FindSkill(skillList,Y_StartIDs)
+                        newOne = FindSkill(specialSkillList,Y_StartIDs)
                     elif id in B_Slots:
-                        newOne = FindSkill(skillList,B_StartIDs)
-                    elif id in ZekeEye:
-                        newOne = FindSkill(skillList,ZekeEyeIDs)
+                        newOne = FindSkill(specialSkillList,B_StartIDs)
+                    elif id in ZekeEyeSlot:
+                        newOne = FindSkill(specialSkillList,ZekeEyeIDs)
                     else:
-                        newOne = random.choice(skillList)
-                    skillList.remove(newOne)
+                        try:
+                            newOne = random.choice(skillList)
+                            skillList.remove(newOne)
+                        except:
+                            pass # Ignore since the last skill chosen wont be able to swap
+                        
                     skillSlot["Name"] = newOne[0]
                     skillSlot["Enhance"] = newOne[1]
             
@@ -56,7 +72,7 @@ def RandomizeSkillEnhancements(optDict):
                         skill = random.choice(ForcedSkills)
                     elif skillSlot["$id"] in ArtsCancelSlots:
                         skill = ArtCancel
-                    elif skillSlot["$id"] in ZekeEye:
+                    elif skillSlot["$id"] in ZekeEyeSlot:
                         skill = EyeOfJustice
                     elif skillSlot["$id"] in X_Slots:
                         skill = XStartBattle
