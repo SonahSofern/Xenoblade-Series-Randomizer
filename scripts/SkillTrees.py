93720c5798f370afcbe260eaa0ac37200d3dd227
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2025-01-08 18:48:29 -0900

    Think i got it

--- scripts/SkillTrees.py
+++ scripts/SkillTrees.py
@@ -16,29 +16,37 @@ def RandomizeSkillEnhancements(optDict):
             skillNameFile = json.load(nameFile)
             
             if isVanilla:
+                def FindSkill(skillList,targetIDs):
+                    for skill in skillList:
+                        if skill["Enhance"] in targetIDs:
+                            return skill
+                ArtCancelEnhanceIDs = [1280, 187, 1309,1340,1368,1397,1431,1450,1464,1494,1523,2246]
+                X_StartIDs = [1277,1327,1321,1369,1389, 1427,1447, 1461,1491,1513]
+                Y_StartIDs = [1290,1308,1349,1360,1398,1419,1448,1474,1483,1521]
+                B_StartIDs = [110,1268,1299,1338,1378,1411,1440,1449,1452,1504,1534]
+                ZekeEyeIDs = [1444]
                 skillList = []
                 for skillSlot in enhanceFile['rows']: # Make a list of ids
                     skillList.append([skillSlot["Name"],skillSlot["Enhance"]])
                     
                 for skillSlot in enhanceFile["rows"]: # Shuffle Ids
-                    newOne = random.choice(skillList)
+                    id = skillSlot["$id"]
+                    if id in ArtsCancelSlots:
+                        newOne = FindSkill(skillList,ArtCancelEnhanceIDs)
+                    elif id in X_Slots:
+                        newOne = FindSkill(skillList,X_StartIDs)
+                    elif id in Y_Slots:
+                        newOne = FindSkill(skillList,Y_StartIDs)
+                    elif id in B_Slots:
+                        newOne = FindSkill(skillList,B_StartIDs)
+                    elif id in ZekeEye:
+                        newOne = FindSkill(skillList,ZekeEyeIDs)
+                    else:
+                        newOne = random.choice(skillList)
                     skillList.remove(newOne)
                     skillSlot["Name"] = newOne[0]
                     skillSlot["Enhance"] = newOne[1]
-                
-                for skillSlot in enhanceFile["rows"]:                #swap around to make the forced skills happy
-                    id = skillSlot["$id"]
-                    if id in ArtsCancelSlots:
-                        pass # do arts cancel stuff
-                    elif id in X_Slots:
-                        pass
-                    elif id in Y_Slots:
-                        pass
-                    elif id in B_Slots:
-                        pass
-                    elif id in ZekeEye:
-                        pass
-
+            
             else:
                 InvalidSkillEnhancements = [ArtCancel, EyeOfJustice, XStartBattle, YStartBattle, BStartBattle, AegisPowerUp, BigBangPowerUp, CatScimPowerUp, VarSaberPowerUp, MechArmsPowerUp, WhipswordPowerUp, DrillShieldPowerUp, DualScythesPowerUp, EvadeDrainHp, EvadeDriverArt, KnuckleClawsPowerUp, BitballPowerUp, GreataxePowerUp, TwinRingPowerUp, MegalancePowerUp,ShieldHammerPowerUp, ChromaKatanaPowerUp, EtherCannonPowerUp, ArtDamageHeal, AegisParty, AegisDriver]
                 ValidSkills = [x for x in EnhanceClassList if x not in InvalidSkillEnhancements]
@@ -61,6 +69,9 @@ def RandomizeSkillEnhancements(optDict):
                     skill.RollEnhancement()
                     skillNameFile["rows"][skillSlot["$id"]-1]["name"] = skill.name
                     enhanceFile["rows"][skillSlot["$id"]-1]["Enhance"] = skill.id
+                    
+                    
+                    
             nameFile.seek(0)
             nameFile.truncate()
             json.dump(skillNameFile, nameFile, indent=2, ensure_ascii=False)
@@ -79,45 +90,3 @@ def ArtsCancelCost():
             driverFiles.truncate()
             json.dump(dFile, driverFiles, indent=2, ensure_ascii=False)
             
-           
-           
- 
-# def SkillShuffle():
-#     ArtCancelEnhanceIDs = [1280, 187, 1309,1340,1368,1397,1431,1450,1464,1494,1523,2246]
-#     X_StartIDs = [1277,1327,1321,1369,1389, 1427,1447, 1461,1491,1513]
-#     Y_StartIDs = [1290,1308,1349,1360,1398,1419,1448,1474,1483,1521]
-#     B_StartIDs = [110,1268,1299,1338,1378,1411,1440,1449,1452,1504,1534]
-#     AlreadySwapped = []
-    
-#     with open(f"./_internal/JsonOutputs/common/BTL_Skill_Dr.json", 'r+', encoding='utf-8') as driverFiles:
-#         dFile = json.load(driverFiles)
-#         skillList = []
-#         for skill in dFile["rows"]:
-#             skillList.append(skill["Enhance"])
-#         print(skillList)
-#         random.shuffle(skillList)
-        
-#         for skill in dFile["rows"]:
-#             _skill = random.choice(skillList)
-#             skill["Enhance"] = _skill
-#             skillList.remove(_skill)
-            
-#         for skill in dFile["rows"]:
-#             if skill["Enhance"] in ArtCancelEnhanceIDs and skill["$id"] not in ArtsCancelSlots:
-#                 aslot = random.choice(ArtsCancelSlots) # Choose a arts cancel slot
-#                 while aslot in AlreadySwapped: # Make sure its not already swapped
-#                     aslot = random.choice(ArtsCancelSlots)
-#                 AlreadySwapped.append(aslot) # Add it to already swapped list
-#                 oldSkillID = skill["$id"] # Set oldskill id to 
-#                 skill["$id"] = aslot
-#                 for __skill in dFile["rows"]:
-#                     if __skill["$id"] == oldSkillID:
-#                         skill[aslot] = oldSkillID 
-#                         break
-                        
-                        
-                
-#         driverFiles.seek(0)
-#         driverFiles.truncate()
-#         json.dump(dFile, driverFiles, indent=2, ensure_ascii=False)
-
