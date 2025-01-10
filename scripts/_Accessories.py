b36a0f1a5ea8aa9e4b72e50bf1eaa7bfe709e454
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2025-01-10 14:07:17 -0900

    Fixed acc names

--- scripts/_Accessories.py
+++ scripts/_Accessories.py
@@ -10,31 +10,30 @@ def RandomizeAccessoryEnhancements():
         with open("./_internal/JsonOutputs/common_ms/itm_pcequip.json", 'r+', encoding='utf-8') as NamesFile: # overflows past a certain num so cant make new names
             enhanceFile = json.load(EnhanceFile)
             NameFile = json.load(NamesFile)
-            
+            prevNames = []
             
             for Acc in enhanceFile["rows"]:
+                
                 enhancement = random.choice(ValidSkills)
+                prevNames.append({"myName" :Acc["Name"], "myEnhance": enhancement})
+                
+                for pair in prevNames: # Ensures the same name has the same enhancement
+                    if pair["myName"] == Acc["Name"]:
+                        enhancement = pair["myEnhance"]
+                        break
+                
                 enhancement.RollEnhancement()
+                
                 Acc["Enhance1"] = enhancement.id
                 Acc["Price"] = (enhancement.Rarity+1) * 5000
                 Acc["Rarity"] = enhancement.Rarity      
                 
-                for skillName in NameFile["rows"]:  # Find old name and make a copy
+                for skillName in NameFile["rows"]:  # Changes names
                     if skillName["$id"] == Acc["Name"]:  
-                        nameid = enhancement.id - 3652 
                         oldName = skillName["name"]
                         oldNameList = oldName.split()
                         lastWord = oldNameList[-1]
-                        newName = f"{enhancement.name} {lastWord}"
-                        
-                        customName = {
-                        "$id": nameid,
-                        "style": 36,
-                        "name": newName
-                        }
-                        Acc["Name"] = nameid + 1
-                        NameFile["rows"].append(customName)
-                        
+                        skillName["name"] = f"{enhancement.name} {lastWord}"                        
                         break
 
                 
