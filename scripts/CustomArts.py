bd73794ac42dc34f21588abd934a053f0b70433a
ofc2000alex <ofc2000alex@gmail.com>
2025-01-20 15:15:30 -0600

    Fixed custom art descriptions

--- scripts/CustomArts.py
+++ scripts/CustomArts.py
@@ -1,6 +1,6 @@
 import json, JSONParser
 import random
-from IDs import Lv1ArtCDs, EnhancementSets, ValidArtIDs, EvasionEnhancementIDs, SpecialEffectArtIDs, AutoAttacks
+from IDs import Lv1ArtCDs, EnhancementSets, ValidArtIDs, EvasionEnhancementIDs, SpecialEffectArtIDs, AutoAttacks, OriginalAOECaptionIDs
 
 def RandomArtReactions(OptionsRunDict):
     HitReactionDistribution = [1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,5,6,7,8,9,10,11,12,13,14]
@@ -25,53 +25,63 @@ def RandomArtReactions(OptionsRunDict):
     if not MultipleReactions:
         JSONParser.ChangeJSONLineWithCallback(["common/BTL_Arts_Dr.json"], ValidArtIDs, RemoveReactionsFromNonLastHit)
 
-def RandomArtCooldowns(): # randomizes art cooldowns
+def RandomArtCooldowns(OptionsRunDict): # randomizes art cooldowns
+    sliderOdds = OptionsRunDict["Art Reactions"]["spinBoxVal"].get()
     with open("./_internal/JsonOutputs/common/BTL_Arts_Dr.json", 'r+', encoding='utf-8') as file:
         data = json.load(file)
         for row in data["rows"]:
             if row["$id"] in ValidArtIDs:
-                row["Recast1"] = random.choice(Lv1ArtCDs)
-                for j in range(2, 7):
-                    row[f"Recast{j}"] = row[f"Recast{j-1}"] - random.choice([0, 0, 0, 1, 1, 2])
-                    if row[f"Recast{j}"] < 1:
-                        row[f"Recast{j}"] = 1
+                if sliderOdds > random.randrange(0,100):
+                    row["Recast1"] = random.choice(Lv1ArtCDs)
+                    for j in range(2, 7):
+                        row[f"Recast{j}"] = row[f"Recast{j-1}"] - random.choice([0, 0, 0, 1, 1, 2])
+                        if row[f"Recast{j}"] < 1:
+                            row[f"Recast{j}"] = 1
         file.seek(0)
         file.truncate()
         json.dump(data, file, indent=2, ensure_ascii=False)
 
-def RandomArtDamageRatios(): # randomizes damage ratios
+def RandomArtDamageRatios(OptionsRunDict): # randomizes damage ratios
+    sliderOdds = OptionsRunDict["Art Reactions"]["spinBoxVal"].get()
     Lv1DamageRatios = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 108, 108, 108, 108, 108, 108, 108, 108, 108, 108, 108, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 125, 125, 130, 130, 130, 130, 130, 130, 140, 140, 140, 140, 144, 144, 144, 144, 144, 144, 144, 150, 150, 150, 150, 150, 150, 150, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 160, 168, 168, 168, 168, 168, 168, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 170, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 210, 210, 210, 210, 210, 210, 210, 210, 210, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 230, 231, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 250, 250, 250, 250, 255, 255, 255, 260, 260, 260, 260, 260, 260, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 270, 285, 285, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 315, 315, 330, 330, 330, 330, 330, 330, 330, 330, 330, 330, 330, 330, 345, 345, 345, 345, 360, 360, 360, 360, 360, 360, 360, 360, 360, 375, 375, 375, 375, 390, 390, 390, 420, 420, 420]
     with open("./_internal/JsonOutputs/common/BTL_Arts_Dr.json", 'r+', encoding='utf-8') as file:
         data = json.load(file)
         for row in data["rows"]:
             if row["$id"] in ValidArtIDs:
-                row["DmgMgn1"] = random.choice(Lv1DamageRatios)
-                for j in range(2, 7):
-                    row[f"DmgMgn{j}"] = row[f"DmgMgn{j-1}"] + random.choice([20, 20, 30, 30, 30, 30, 30, 30, 40, 40, 40, 50, 50])
+                if sliderOdds > random.randrange(0,100):
+                    row["DmgMgn1"] = random.choice(Lv1DamageRatios)
+                    for j in range(2, 7):
+                        row[f"DmgMgn{j}"] = row[f"DmgMgn{j-1}"] + random.choice([20, 20, 30, 30, 30, 30, 30, 30, 40, 40, 40, 50, 50])
         file.seek(0)
         file.truncate()
         json.dump(data, file, indent=2, ensure_ascii=False)
 
-def RandomArtEnhancements(): # randomizes art enhancements
+def RandomArtEnhancements(OptionsRunDict): # randomizes art enhancements
+    sliderOdds = OptionsRunDict["Art Reactions"]["spinBoxVal"].get()
     with open("./_internal/JsonOutputs/common/BTL_Arts_Dr.json", 'r+', encoding='utf-8') as file:
         data = json.load(file)
         for row in data["rows"]:
             if row["$id"] in ValidArtIDs:
-                if row["$id"] not in SpecialEffectArtIDs:
-                    row["ArtsBuff"] = 0
-                SelectedEnhancementList = random.choice(EnhancementSets)
-                for j in range(1, 7):
-                    row[f"Enhance{j}"] = SelectedEnhancementList[j-1]
-                if row["Enhance1"] in EvasionEnhancementIDs: # Evasion Art Enhancement
-                    row["ArtsBuff"] = 2
+                if sliderOdds > random.randrange(0,100):
+                    if row["$id"] not in SpecialEffectArtIDs:
+                        row["ArtsBuff"] = 0
+                    SelectedEnhancementList = random.choice(EnhancementSets)
+                    for j in range(1, 7):
+                        row[f"Enhance{j}"] = SelectedEnhancementList[j-1]
+                    if row["Enhance1"] in EvasionEnhancementIDs: # Evasion Art Enhancement
+                        row["ArtsBuff"] = 2
         file.seek(0)
         file.truncate()
         json.dump(data, file, indent=2, ensure_ascii=False)
 
-
 def GenCustomArtDescriptions():
     RangeType = {
         "AOE" : [3,5]
+    }
+
+    MoveType = {
+        "Heal": [3],
+        "Defend": [11]
     }
 
     Reactions = {
@@ -81,6 +91,15 @@ def GenCustomArtDescriptions():
         "S" : [4],
         "Kb": [5,6,7,8,9],
         "Bd": [10,11,12,13,14]
+    }
+
+    FullReactions = {
+        "Break" : [1],
+        "Topple" : [2],
+        "Launch" : [3],
+        "Smash" : [4],
+        "Knockback": [5,6,7,8,9],
+        "Blowdown": [10,11,12,13,14]
     }
 
     Enhancements = {
@@ -102,7 +121,7 @@ def GenCustomArtDescriptions():
         "HP Potion": [2815,2860],
         "Insect↑": [2685],
         "Launch↑": [2780,2775],
-        "LowHP↑": [2790,2785],
+        "Low HP↑": [2790,2785],
         "Machine↑": [2730,2725],
         "Pierce": [2861],
         "Side↑": [2746,2745,2750],
@@ -131,7 +150,7 @@ def GenCustomArtDescriptions():
         "P Def↓": [23],
         "E Def↓": [24],
         "Res↓": [25],
-        "Stop": [30],
+        "Freeze": [30],
         "Enrage": [35]
     }
     with open("./_internal/JsonOutputs/common/BTL_Arts_Dr.json", "r+", encoding='utf-8') as ArtsFile:     
@@ -143,55 +162,91 @@ def GenCustomArtDescriptions():
                 if art["Caption"] == 0: # Only change things that have captions
                     continue
                 targetCaptionID = art["Caption"]
-                rangeCaption = ""
-                
+                CombinedCaption = ["","","","",""]
+
                 # AOE
                 for key,values in RangeType.items():    
                     if art["RangeType"] in values:
-                        rangeCaption += f"{key} / "
+                        CombinedCaption[0] += f"{key}"
                         break
-                    
+
+                # Type of Art
+                for key,values in MoveType.items():
+                    if art["ArtsType"] in values:
+                        CombinedCaption[1] += f"{key}"
+                        break
+
                 # Reactions 
-                reactionCaption = ""
+                ReactCaption = ""
+                TypeReactions = []
                 for i in range(1,17):              
-                    if art[f"HitDirID{i}"] == 0:
-                        break
-                    else:
+                    if art[f"HitDirID{i}"] != 0:
                         for key,values in Reactions.items():
                             if art[f"ReAct{i}"] in values:
-                                reactionCaption += f"{key}/"
+                                ReactCaption += f"{key}->"
+                                TypeReactions.append(art[f"ReAct{i}"])
                                 break
-                reactionCaption = reactionCaption[:-1]
-                reactionCaption += " / "
+                ReactCaption = ReactCaption[:-2]
+                if len(ReactCaption) > 15: # If the length is too long, shorten it to "Driver Combo"
+                    ReactCaption = "Driver Combo"
+                elif len(TypeReactions) == 1: # If the length is 1, spell out the reaction
+                    for key,values in FullReactions.items():
+                        if TypeReactions[0] in values:
+                            ReactCaption = f"{key}"
+                            break
+                CombinedCaption[2] = ReactCaption
                     
                 # Enhancements
-                enhancementCaption = ""
+                EnhanceCaption = ""
                 for key,values in Enhancements.items():
                     if art["Enhance1"] in values:
-                        enhancementCaption += f"{key} / "
+                        CombinedCaption[3] += f"{key}"
                         break
                     
                 # Debuffs  
-                debuffCaption = ""                      
+                DebuffCaption = ""                      
                 for key,values in Debuffs.items():
                     if art["ArtsDeBuff"] in values:
-                        debuffCaption = f"{key}"
+                        CombinedCaption[4] = f"{key}"
                         break
-                  
+
+                # Putting it all together
+                TotalArtDescription = ""    
+                for i in range(0, len(CombinedCaption)):
+                    if CombinedCaption[i] != "":
+                        FirstDescriptionMod = i
+                        break
+                for i in range(len(CombinedCaption) - 1, 0, -1):
+                    if CombinedCaption[i] != "":
+                        LastDescriptionMod = i
+                        break
+                TotalArtDescription += CombinedCaption[FirstDescriptionMod]
+                if FirstDescriptionMod != LastDescriptionMod:
+                    for i in range(FirstDescriptionMod + 1, LastDescriptionMod + 1):
+                        if CombinedCaption[i] != "":
+                            TotalArtDescription += " / "
+                            TotalArtDescription += CombinedCaption[i]
+
                 # Update Descriptions  
                 for desc in descData["rows"]:            
                     if desc["$id"] == targetCaptionID:
-                        desc["name"] = f"{rangeCaption}{reactionCaption}{enhancementCaption}{debuffCaption}"  
-                        print(desc["name"])
-                        art
+                        desc["name"] = TotalArtDescription 
                         break
+
+            # Fix for Anchor Shot updating description number past Urayan story flags        
+            for row in descData["rows"]:
+                if row["$id"] == 4:
+                    RexAnchorShotCaption = row["name"]
+                if row["$id"] == 5:
+                    row["name"] = RexAnchorShotCaption
+                    break
+
             DescFile.seek(0)
             DescFile.truncate()
             json.dump(descData, DescFile, indent=2, ensure_ascii=False)             
         ArtsFile.seek(0)
         ArtsFile.truncate()
         json.dump(artsData, ArtsFile, indent=2, ensure_ascii=False)
-    
 
 def RemoveReactionsFromNonLastHit(art):
     last_hit = -1
