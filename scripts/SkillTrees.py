70d9ceb32bd56d87c37eadd03bae326f9e458274
ofc2000alex <ofc2000alex@gmail.com>
2025-01-20 17:18:27 -0600

    added options to randomize placement of arts cancel and x,y,b starting arts

--- scripts/SkillTrees.py
+++ scripts/SkillTrees.py
@@ -1,33 +1,55 @@
 import json, random
 from Enhancements import *
 
-
-
 def RandomizeSkillEnhancements(optDict):
-    ArtsCancelSlots = [12,55,82,102,131,161]
-    ZekeEyeSlot = [152]
-    X_Slots= [1,51,61,91,121,162]
-    Y_Slots= [11,31,71,101,141,171]
-    B_Slots= [21,52,62,92,122,151]
+    ArtsCancelSlots, X_Slots, Y_Slots, B_Slots = [], [], [], []
+    Rex, Nia, Tora, Vandham, Morag, Zeke = Helper.InclRange(1, 30), Helper.InclRange(31, 60), Helper.InclRange(61, 90), Helper.InclRange(91, 120), Helper.InclRange (121, 150), Helper.InclRange(151, 180)
+    DriverList = [Rex, Nia, Tora, Vandham, Morag, Zeke]
+    for Driver in DriverList:
+        RemainingUnusedDriverIDs = Driver.copy()
+        if not optDict["Driver Skill Trees"]["subOptionObjects"]["Tier 1: Arts Cancel"]["subOptionTypeVal"].get():
+            ChosenID = 0
+            ChosenID = random.choice(RemainingUnusedDriverIDs)
+            ArtsCancelSlots.append(ChosenID)
+            RemainingUnusedDriverIDs.remove(ChosenID)
+        if not optDict["Driver Skill Trees"]["subOptionObjects"]["Tier 1: Start With Attack"]["subOptionTypeVal"].get():
+            ChosenIDs = random.sample(RemainingUnusedDriverIDs, 3)
+            X_Slots.append(ChosenIDs[0])
+            RemainingUnusedDriverIDs.remove(ChosenIDs[0])
+            Y_Slots.append(ChosenIDs[1])
+            RemainingUnusedDriverIDs.remove(ChosenIDs[1])
+            B_Slots.append(ChosenIDs[2])
+            RemainingUnusedDriverIDs.remove(ChosenIDs[2])
+        if Driver == Zeke:
+            ChosenID = 0
+            ZekeEyeSlot = [random.choice(RemainingUnusedDriverIDs)]
+    if not ArtsCancelSlots:
+        ArtsCancelSlots = [12,55,82,102,131,161]
+        FirstSlotCost()
+    if not X_Slots:
+        X_Slots = [1,51,61,91,121,162]
+        Y_Slots = [11,31,71,101,141,171]
+        B_Slots = [21,52,62,92,122,151]
+
     isVanilla = not optDict["Driver Skill Trees"]["subOptionObjects"]["Nonstandard Skills"]["subOptionTypeVal"].get()
     with open("./_internal/JsonOutputs/common/BTL_Skill_Dr.json", 'r+', encoding='utf-8') as enhancementFile:
         with open("./_internal/JsonOutputs/common_ms/btl_skill_dr_name.json", 'r+', encoding='utf-8') as nameFile:
             enhanceFile = json.load(enhancementFile)
             skillNameFile = json.load(nameFile)
-                                    # ArtCancelEnhanceIDs.remove(newOne[1])
+
             if isVanilla:
-                def FindSkill(skillList,targetEnhancementIDs):
+                def FindSkill(skillList, targetEnhancementIDs):
                     for skill in skillList:
                         if skill[1] in targetEnhancementIDs:
                             targetEnhancementIDs.remove(skill[1])
                             skillList.remove(skill)
                             return skill
-                ArtCancelEnhanceIDs = [1280, 187, 1309,1340,1368,1397,1431,1450,1464,1494,1523,2246]
-                X_StartIDs = [1277,1327,1321,1369,1389, 1427,1447, 1461,1491,1513]
+                ArtCancelEnhanceIDs = [1280,187,1309,1340,1368,1397,1431,1450,1464,1494,1523,2246]
+                X_StartIDs = [1277,1327,1321,1369,1389,1427,1447,1461,1491,1513]
                 Y_StartIDs = [1290,1308,1349,1360,1398,1419,1448,1474,1483,1521]
                 B_StartIDs = [110,1268,1299,1338,1378,1411,1440,1449,1452,1504,1534]
-                ZekeEyeIDs = [1444]
-                AllSpecialEnhances = ArtCancelEnhanceIDs + X_StartIDs + Y_StartIDs + B_StartIDs + ZekeEyeIDs
+                ZekeEyeID = [1444]
+                AllSpecialEnhances = ArtCancelEnhanceIDs + X_StartIDs + Y_StartIDs + B_StartIDs + ZekeEyeID
                 skillList = []
                 specialSkillList = []
                 
@@ -39,7 +61,6 @@ def RandomizeSkillEnhancements(optDict):
                         
                 random.shuffle(skillList)
                 random.shuffle(specialSkillList)
-
                     
                 for skillSlot in enhanceFile["rows"]:  # Set special Cases
                     id = skillSlot["$id"]
@@ -52,7 +73,7 @@ def RandomizeSkillEnhancements(optDict):
                     elif id in B_Slots:
                         newOne = FindSkill(specialSkillList,B_StartIDs)
                     elif id in ZekeEyeSlot:
-                        newOne = FindSkill(specialSkillList,ZekeEyeIDs)
+                        newOne = FindSkill(specialSkillList,ZekeEyeID)
                     else:
                         try:
                             newOne = random.choice(skillList)
@@ -62,10 +83,8 @@ def RandomizeSkillEnhancements(optDict):
                         
                     skillSlot["Name"] = newOne[0]
                     skillSlot["Enhance"] = newOne[1]
-            
             else:
-                
-                InvalidSkillEnhancements = [CritBoost, FlatCritBoost, PartyCritMaxAffinity, DamageAndCritUpMaxAffinity,ForcedHPPotionOnHit,HpPotChanceFor2,ArtCancel, EyeOfJustice, XStartBattle, YStartBattle, BStartBattle, AegisPowerUp, BigBangPowerUp, CatScimPowerUp, VarSaberPowerUp, MechArmsPowerUp, WhipswordPowerUp, DrillShieldPowerUp, DualScythesPowerUp, EvadeDrainHp, EvadeDriverArt, KnuckleClawsPowerUp, BitballPowerUp, GreataxePowerUp, TwinRingPowerUp, MegalancePowerUp,ShieldHammerPowerUp, ChromaKatanaPowerUp, EtherCannonPowerUp, ArtDamageHeal, AegisParty, AegisDriver]
+                InvalidSkillEnhancements = [CritBoost, FlatCritBoost, PartyCritMaxAffinity, DamageAndCritUpMaxAffinity, ForcedHPPotionOnHit, HpPotChanceFor2,ArtCancel, EyeOfJustice, XStartBattle, YStartBattle, BStartBattle, AegisPowerUp, BigBangPowerUp, CatScimPowerUp, VarSaberPowerUp, MechArmsPowerUp, WhipswordPowerUp, DrillShieldPowerUp, DualScythesPowerUp, EvadeDrainHp, EvadeDriverArt, KnuckleClawsPowerUp, BitballPowerUp, GreataxePowerUp, TwinRingPowerUp, MegalancePowerUp, ShieldHammerPowerUp, ChromaKatanaPowerUp, EtherCannonPowerUp, ArtDamageHeal, AegisParty, AegisDriver]
                 ValidSkills = [x for x in EnhanceClassList if x not in InvalidSkillEnhancements]
                 ForcedSkills = []
                 for skillSlot in enhanceFile['rows']:
@@ -87,28 +106,19 @@ def RandomizeSkillEnhancements(optDict):
                     skill.RollEnhancement()
                     skillNameFile["rows"][skillSlot["$id"]-1]["name"] = skill.name
                     enhanceFile["rows"][skillSlot["$id"]-1]["Enhance"] = skill.id
-                    
-                    
-                    
             nameFile.seek(0)
             nameFile.truncate()
             json.dump(skillNameFile, nameFile, indent=2, ensure_ascii=False)
         enhancementFile.seek(0)
         enhancementFile.truncate()
         json.dump(enhanceFile, enhancementFile, indent=2, ensure_ascii=False)
-        
 def FirstSlotCost(): # Used since art cancel gets put here
-    for i in range(1,7):
+    for i in range(1, 7):
         with open(f"./_internal/JsonOutputs/common/BTL_Skill_Dr_Table0{i}.json", 'r+', encoding='utf-8') as driverFiles:
             dFile = json.load(driverFiles)
-            
             for item in dFile["rows"]:
                 if item["Round"] == 1 and item["ColumnNum"] == 1 and item["RowNum"] == 1: 
                     item["NeedSp"] = 0
-                    
             driverFiles.seek(0)
             driverFiles.truncate()
             json.dump(dFile, driverFiles, indent=2, ensure_ascii=False)
-            
-def EarlyArtsCancel():
-    pass
