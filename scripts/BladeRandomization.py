4541414620c4b9d8e231db702afedde1f6e3d756
MackChabot <mackenziechabot@gmail.com>
2025-01-25 17:47:17 -0500

    I think I have something working regarding cutscene crashes. Needs more testing but I don't want to lose this.

--- scripts/BladeRandomization.py
+++ scripts/BladeRandomization.py
@@ -40,26 +40,18 @@ from IDs import ValidEnemyPopFileNames
 
 
 #TODO: Bugs
-# 1. Rex cannot use Poppi arts (obviously)
-# 1a. When switching off of Poppi, Rex still can not attack (even with Godfrey)
-# 2. Using an unsupported blade (Crossette on Tora for example) during a chain attack causes a softlock
 # 3. After the scene in Elpys where Pandoria's glasses become transparent, Corvin's sprite in the menu (and only the menu) becomes Pandoria again (with the transparent glasses). Everything else is still Corvin
 # 3a. After this scene, Corvin should still appear as Corvin. Pandoria (on Rex) should now appear with transparent glasses
 # 4. Since it's based on poppiswap, Poppi has no abilities on her specials
-# 4a. Special description should say something like "Set Crossette's Poppiswap". Reference the original wording
 # 5. No combination of Godfrey/Wulfric or Pyra/Nia's do the special tier 4.
 
 
 #TODO: Things to try
-# 1. Add a cosmetic item for Pandoria's hooded form
-# 2. If Pyra was not randomized to a tank, make sure Poppi gets randomized to a tank. Otherwise, Poppi can be whatever. Basically, make sure there is at least one tank.
 # 3. Can I use poppiswap on Crossette to modify Poppi? (cannot be tested until Poppi and Crossette get arts for other characters)
-# 4a. Can I modify the menu to put Poppiswap on Poppi so Tora doesn't have the poppiswap anymore?
-# 5. Rename items which summon blades. For example, if Kasandra becomes Mythra, then rename "Lucky Core Crystal" to "Aegis Core Crystal" or something
-# 6. Can Pneuma be randomized? I think she's too broken to have normally, but we could nerf her and buff whoever replaces her
 
-#TODO: Figure out the two different Dagas forms? My randomization creates Dagas 1 I believe
+# TODO: ChangeJSONFileWithCallback() could probably be renamed to be clearer.
 
+# TODO: all functions which actually modify the tables should be renamed to start with "apply". This will differentiate between determining the calculations and applying them
 
 OriginalBlades = dict() # Populated in PopulateBlades()
 BladeNames = dict() # Populated in PopulateBlades()
@@ -67,6 +59,7 @@ Original2Replacement = dict() # Populated in RandomizeBlades()
 Replacement2Original = dict() # Populated in RandomizeBlades()
 OriginalGacha = dict() # Populated in PopulateGacha()
 
+# TODO: Is this used? If not, should at least be documented here
 BladesWhichCantBeRandomizedAtAll = [1004, 1008]  # Dromarch and Roc cannot be randomized because they are non-human and driver-exclusive
 
 # Keep track of blades which only have arts defined for specific drivers. These blades can appear in gacha, but an overdrive to a compatible driver would be needed to actually use them. They cannot replace the default blade of another driver, as that driver would not be able to use them
@@ -74,13 +67,12 @@ RexExclusiveBlades = [1001, 1002, 1011]  # Pyra, Mythra, Nia
 MoragExclusiveBlades = [1009]  # Brighid
 ZekeExclusiveBlades = [1010]  # Pandoria
 
+# TODO: Is this used?
 ToraExclusiveBlades = [1005, 1006, 1007]  # Poppi
 
-AllHumanoidBladesBesidesPoppi = [1001, 1002, 1009, 1010, 1011, 1014, 1015, 1016, 1017, 1018, 1019, 1020, 1023, 1024, 1025, 1026, 1027, 1028, 1030, 1031, 1033, 1034, 1035, 1036, 1037, 1038, 1039, 1041, 1043, 1044, 1045, 1046, 1047, 1048, 1050, 1076, 1104, 1105, 1106, 1107, 1108, 1109, 1111] # Note: Dagas has two entries, 1022 and 1050. The former is his original form, the latter is his true form. We are randomizing the true form into the rotation, NOT the original form.
-HumanoidBladesThatArentDriverExclusive = [1014, 1015, 1016, 1017, 1018, 1019, 1020, 1023, 1024, 1025, 1026, 1027, 1028, 1030, 1031, 1033, 1034, 1035, 1036, 1037, 1038, 1039, 1041, 1043, 1044, 1045, 1046, 1047, 1048, 1050, 1076, 1104, 1105, 1106, 1107, 1108, 1109, 1111]
+AllHumanoidBladesBesidesPoppi = [1001, 1002, 1009, 1010, 1011, 1014, 1015, 1029, 1016, 1017, 1018, 1019, 1020, 1023, 1024, 1025, 1026, 1027, 1028, 1030, 1031, 1033, 1034, 1035, 1036, 1037, 1038, 1039, 1041, 1043, 1044, 1045, 1046, 1047, 1048, 1050, 1076, 1104, 1105, 1106, 1107, 1108, 1109, 1111] # Note: Dagas has two entries, 1022 and 1050. The former is his original form, the latter is his true form. We are randomizing the true form into the rotation, NOT the original form.
+HumanoidBladesThatArentDriverExclusive = [1014, 1015, 1016, 1017, 1018, 1019, 1020, 1023, 1024, 1025, 1026, 1027, 1028, 1030, 1031, 1033, 1034, 1035, 1036, 1037, 1038, 1039, 1041, 1043, 1044, 1045, 1046, 1047, 1048, 1050, 1076, 1104, 1105, 1106, 1107, 1108, 1109, 1111] # TODO: Is this used?
 NonHumanoidBlades = [1021, 1029, 1032, 1040, 1049]  # Boreas, Ursula, Sheba, Herald, Sever (Race is not 1) (Note: There's 2 Severs, 1042 and 1049. I believe the former is enemy and latter is player)
-
-# TODO: Handle the randomization for Boreas, Ursula, Sheba, Herald, and Sever separetely. They cannot be default blades, but they can remain as either Gacha or item summons. The gacha tables and item summons can be randomized.
 
 def ModifyGachaAvailability(gacha):
     if gacha['Condition'] == 1789:  # NG+
@@ -103,7 +95,19 @@ def InitialSetup():
     JSONParser.ChangeJSONFileWithCallback(["common/BLD_RareList.json"], PopulateGacha)
     JSONParser.ChangeJSONFileWithCallback(["common/BTL_Arts_Dr.json"], MakeAllArtsAccessible)
 
+# side is either L or R (case doesn't matter). Could also be the words 'left' and 'right', I don't really care
+def WeaponType2Resource(weapon_type, side):
+    TwoHandedWeapons = [3, 5, 7, 9, 16, 22, 23, 25, 26, 30, 32, 34, 35]
+
+    base_string = 'wp' + str(weapon_type).zfill(2) + '0101'
+
+    if weapon_type in TwoHandedWeapons:
+        return base_string + '_' + side[0].lower()
+    else:
+        return base_string
+
 def BladeRandomization(OptionsRunDict):
+
     #TODO: Use suboptions?
     #TODO: Include console messages
 
@@ -120,9 +124,24 @@ def BladeRandomization(OptionsRunDict):
     #    Note: I thought of locking Pandoria to a side quest crystal which you get after fighting Zeke (Theory or Herald) but that doesn't work because you fight Zeke in chapter 10.
     JSONParser.ChangeJSONFileWithCallback(["common/CHR_EnArrange.json"], ReplaceEnemyBlades)
 
+    BugFixes()
+
+    FinalTouches()
+
+    #search_bdat("R")
+    #search_bdat("L")
+    #search_bdat("Nia")
+    #search_bdat("Pyra")
+    #search_bdat("Mythra")
+    #search_bdat("Pandoria")
+    #search_bdat("wp070101")
+
 def ReplaceBlade(blade, replace_with_id):
+    # Note: DefWeapon changes their initial arts. However, it is needed because cutscenes can crash if the wrong weapon is loaded
+    # I do not know if this affects every weapon or not, but I suspect it has something to do with Brighid's
     for key, value in blade.items():
-        if key in ['$id', 'ReleaseLock', 'CreateEventId']:
+        #if key in ['$id', 'ReleaseLock', 'DefWeapon']:
+        if key in ['$id', 'ReleaseLock']:
             continue
         blade[key] = OriginalBlades[replace_with_id][key]
 
@@ -137,7 +156,12 @@ def canBeReplaced(original, replacement):
     # Note: Rex can be the master driver, so it's fine if his blades get randomized to whoever
 
     if original == 1009: #Brighid
-        return not (replacement in RexExclusiveBlades or replacement in ZekeExclusiveBlades)
+        #return replacement == 1027 # Force Brighid to be Kora, for testing (Works)
+        #return replacement == 1045 # Force Brighid to be Mikhail, for testing (Works)
+        #return replacement == 1029 # Force Brighid to be Ursula, for testing (Works)
+        return replacement == 1016 # Force Brighid to be Wulfric, for testing (Crashes in cutscene)
+        #return not (replacement in RexExclusiveBlades or replacement in ZekeExclusiveBlades)
+        #return replacement == 1009 # Don't randomize brighid, for now
     if original == 1014: #Aegeaon
         return not (replacement in RexExclusiveBlades or replacement in ZekeExclusiveBlades)
     if original == 1010: #Pandoria
@@ -180,7 +204,7 @@ def RandomizeBlades(OptionsRunDict):
     JSONParser.ChangeJSONFileWithCallback(["common/CHR_Bl.json"], ReplaceBlades)
 
 def RandomizePoppiForms(OptionsRunDict):
-    print('RandomizePoppiForms()')
+    print('TODO: RandomizePoppiForms()')
     # TODO: Randomize Poppi
     # TODO: How do I make it so the poppiswaps are in the right spot
 
@@ -202,11 +226,10 @@ def RandomizeGachaTable(OptionsRunDict):
         next_replacement = randomized_order[0]
 
         original_gacha_blade = OriginalGacha[next_gacha]
-
         next_gacha_blade = Original2Replacement[OriginalGacha[next_gacha]]
 
         # Replace original Dagas with true form Dagas in the gacha table
-        if next_replacement == 7:
+        if next_replacement == 7: # TODO: Is this check still needed? I think he's swapped already when populating OriginalGacha
             next_replacement_blade = 1050
         else:
             next_replacement_blade = Original2Replacement[OriginalGacha[next_replacement]]
@@ -229,8 +252,9 @@ def ReplaceGacha(gacha):
     gacha['Blade'] = OriginalGacha2ReplacementBlade[gacha['$id']]
 
 def ReplaceEnemyBlades(enemy):
-    if enemy['BladeID'] in Original2Replacement:
-        enemy['BladeID'] = Original2Replacement[enemy['BladeID']]
+    x = 5
+    #if enemy['BladeID'] in Original2Replacement:
+    #    enemy['BladeID'] = Original2Replacement[enemy['BladeID']]
 
     # TODO: Do this with all blades
     #if enemy['BladeID'] == Pandoria:
@@ -241,11 +265,11 @@ def MakeAllArtsAccessible(art):
     # In a normal playthrough, these arts only really matter for Aegis Sword, as that affects the
     # Junk Sword arts. However, a handful of arts have high enough requirements that Rex can't use
     # it if randomization gives him Brighid or Pandoria early on. For simplicity and potential
-    # future-proofing, just cap all arts at 4. This is Rex's minimum level when he gets his first blade.
-    # This is chosen instead of 1 just so the Junk Sword still behaves as intended
+    # future-proofing, just cap all arts at 3. This is Rex's minimum level when he gets his first blade.
+    # This is chosen instead of 1 just so the Junk Sword still behaves as intended prior to getting Pyra
     for i in Helper.InclRange(1,5):
-        if art['ReleaseLv' + str(i)] > 4:
-            art['ReleaseLv' + str(i)] = 4
+        if art['ReleaseLv' + str(i)] > 3:
+            art['ReleaseLv' + str(i)] = 3
 
 def FullSpeedAhead(deed):  # increases movement speed from the deed
     deed['Value'] = 10000
@@ -288,3 +312,204 @@ def PopulateGacha(gacha):
 #     #accessory['Blade'] = Kasandra
 #     print(accessory)
 
+def BugFixes():
+    def FixCutsceneCrashForNotHavingTwoWeapons(weapon):
+        # TODO: This doesn't seem to work.
+        #  Instead, try hardcoding a valid weapon instead of using the offhand one
+        #  (I'm wondering if there's something specific where a left handed weapon
+        #  just simply cannot be used in the right hand and vice versa)
+        print('')
+        #weapon['RscR'] = 121
+        #weapon['RscL'] = 120
+        #if weapon['RscR'] == 0:
+        #    weapon['RscR'] = weapon['RscL']
+        #if weapon['RscL'] == 0:
+        #    weapon['RscL'] = weapon['RscR']
+
+    def FixCutsceneCrashForNotHavingTwoWeapons2(cutscene):
+        # TODO: This doesn't seem to work.
+        #  Instead, try hardcoding a valid weapon instead of using the offhand one
+        #  (I'm wondering if there's something specific where a left handed weapon
+        #  just simply cannot be used in the right hand and vice versa)
+        print(cutscene)
+
+        # TODO: Doesn't crash
+        #if (cutscene['$id'] == 13):
+        #    cutscene['resourceL'] = 'wp090101_l' # Dual Scythes, I believe
+        #    cutscene['resourceR'] = 'wp090101_r' # Dual Scythes, I believe
+
+        original = cutscene['blade']
+
+        # Bail if this blade was not randomized
+        if original not in Original2Replacement:
+            return
+
+        replacement = Original2Replacement[original]
+
+        print("======================= Resource Modifications")
+        print("Original Blade: " + BladeNames[original])
+        print("Original Weapon Type" + str(OriginalBlades[original]['WeaponType']))
+        print("Original resource (R): " + cutscene['resourceR'])
+        print("Original resource (L): " + cutscene['resourceL'])
+        print("----")
+        print("New Blade: " + BladeNames[replacement])
+        print("New Weapon Type" + str(OriginalBlades[replacement]['WeaponType']))
+        print("New Resource (R): " + WeaponType2Resource(OriginalBlades[replacement]['WeaponType'], 'R'))
+        print("New resource (L): " + WeaponType2Resource(OriginalBlades[replacement]['WeaponType'], 'L'))
+        print("=======================")
+
+        # TODO: Works
+        if (cutscene['$id'] == 13):
+            cutscene['resourceL'] = 'wp110101' # Megalance, I believe
+            cutscene['resourceR'] = 'wp110101' # Megalance, I believe
+
+        # TODO: Crashes
+        #if (cutscene['$id'] == 12):
+        #    cutscene['resourceL'] = 'wp090101_l' # Dual Scythes, I believe
+        #    cutscene['resourceR'] = 'wp090101_r' # Dual Scythes, I believe
+
+        # If there is a weapon not defined, just replace it with a known, valid resource (whipsword in this case)
+        # This can happen in cases of dual wielded weapons being replaced with single-hand weapons,
+        # as the cutscene will try to load the off-hand weapon, which is not defined
+        #if cutscene['resourceR'] == '':
+        #    cutscene['resourceR'] = 'wp090101_r'
+        #if cutscene['resourceL'] == '':
+        #    cutscene['resourceL'] = 'wp090101_l'
+
+        #if cutscene['resourceR'] == '':
+        #    cutscene['resourceR'] = weapon['resourceL']
+        #if cutscene['resourceL'] == '':
+        #    cutscene['resourceL'] = weapon['resourceR']
+
+
+    JSONParser.ChangeJSONFileWithCallback(["common/ITM_PcWpn.json"], FixCutsceneCrashForNotHavingTwoWeapons)
+    #JSONParser.ChangeJSONFileWithCallback(["common/BTL_Wpn_En.json"], FixCutsceneCrashForNotHavingTwoWeapons)
+    JSONParser.ChangeJSONFileWithCallback(["common/EVT_cutscene_wp.json"], FixCutsceneCrashForNotHavingTwoWeapons2)
+
+# TODO: Having all ITM_PcWpn be 121 and 120 while also having all EVT_cutscene_wp set to wp090101_r and wp090101_l seems to work.
+# TODO: Having all ITM_PcWpn be 121 and 120 while NOT having all EVT_cutscene_wp set to wp090101_r and wp090101_l causes a crash
+# TODO: NOT having ITM_PcWpn be 121 and 120 while having all EVT_cutscene_wp set to wp090101_r and wp090101_l seems to work
+
+
+
+
+def FinalTouches():
+    print('FinalTouches')
+    # common_ms/menu_tutorial.html:
+    # ID 320 name is "Nia has unleashed her true power! Now Rex can [System:Color name=tutorial ]engage Nia as a Blade[/System:Color]."
+    # ID 344 name is "When you want to engage Nia as a Blade, go to [System:Color name=tutorial ]Main Menu[/System:Color] > [System:Color name=tutorial ]Characters[/System:Color], [System:Color name=tutorial ]then select the Driver Nia and press [ML:icon icon=Y ][/System:Color]."
+    # ID 345 name is "When you want to travel with Nia in her Driver form, go to [System:Color name=tutorial ]Main Menu[/System:Color] > [System:Color name=tutorial ]Characters[/System:Color], [System:Color name=tutorial ]then select the Blade Nia and press [ML:icon icon=Y ][/System:Color]."
+    # IDs 401-403 talks about the Pyra/Nia special. Replace this with something saying that it doesn't work
+    # Various IDs mention other blades such as Poppibuster, Shulk, Fiora, and Elma
+    # 497: "Players who use Nia in her Blade form may have to return her to Driver form to be able to use Fiora as a Blade."
+    # Honestly, just comb this whole file searching for the name of any blade
+
+    # common/MNU_CmnWindow.html
+    # 183-188 are pyra/mythra/nia things
+    #223 has Nia
+
+    #TODO: Can I just loop over each table of interest, search for each blade name, and replace that string if it exists?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_hyperlinks(base_url):
+    """Fetch all hyperlinks from the base webpage."""
+    response = requests.get(base_url)
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch base URL: {response.status_code}")
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = []
+
+    for a_tag in soup.find_all('a', href=True):
+        links.append(a_tag['href'].replace('\\', '/'))  # Correct backslashes to forward slashes
+
+    return links
+
+def search_tables_in_pages(base_url, links, search_string):
+    """Search for the given string in the tables of each linked webpage."""
+    matching_pages = []
+
+    for link in links:
+        full_url = base_url.rstrip('/') + '/' + link
+        response = requests.get(full_url)
+
+        if response.status_code != 200:
+            print(f"Failed to fetch {full_url}: {response.status_code}")
+            continue
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        tables = soup.find_all('table')
+
+        for table in tables:
+            # Get all hyperlinks in the table
+            hyperlinks = [a.get_text().lower() for a in table.find_all('a')]
+
+            # Check if search_string is in table text but not in any hyperlink text
+            table_text = table.get_text().lower()
+            if search_string.lower() in table_text and all(search_string.lower() not in link for link in hyperlinks):
+                matching_pages.append(full_url)
+                print(full_url)
+                break  # No need to check more tables on this page
+
+    return matching_pages
+
+def search_table_headers_in_pages_ends_with(base_url, links, search_string):
+    """Search for the given string in the tables of each linked webpage."""
+    matching_pages = []
+
+    for link in links:
+        full_url = base_url.rstrip('/') + '/' + link
+        response = requests.get(full_url)
+
+        if response.status_code != 200:
+            print(f"Failed to fetch {full_url}: {response.status_code}")
+            continue
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        headers = soup.find_all('th')
+
+        for header in headers:
+           # Check if header text ends with the search string
+            if header.get_text().endswith(search_string):
+                matching_pages.append(full_url)
+                break  # No need to check more tables on this page
+
+    return matching_pages
+
+def search_bdat(search_string):
+    base_url = "https://xenoblade.github.io/xb2/bdat/index.html"
+
+    print('Search String: ' + search_string)
+
+    try:
+        print("Fetching hyperlinks...")
+        links = fetch_hyperlinks(base_url)
+        print(f"Found {len(links)} links.")
+
+        #matching_pages = search_tables_in_pages(base_url.rsplit('/', 1)[0], links, search_string)
+        matching_pages = search_table_headers_in_pages_ends_with(base_url.rsplit('/', 1)[0], links, search_string)
+
+        if matching_pages:
+            print(f"Found the string on the following pages:")
+            for page in matching_pages:
+                print(page)
+        else:
+            print("No pages contain the given search string outside hyperlinks.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
