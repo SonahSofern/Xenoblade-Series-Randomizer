498d946b028c8e4edab40572b0f0b395ae1d7d42
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2025-02-01 09:59:09 -0900

    Trying to fix wep rando

--- scripts/_BladeWeapons.py
+++ scripts/_BladeWeapons.py
@@ -1,6 +1,8 @@
-import json, random, Helper
+import json, random, Helper, JSONParser
 
 WeaponsList = [] # Once the torna blades get their weapons fixed and allowed to be upgraded we can put them here
+
+# Cant get specials to work ):
 
 # Predefined Special Cases
 AegisSwordData = {"id": 1, "DefWeapon": 5001}
@@ -43,8 +45,8 @@ class Weapon:
             self.Replacements.append(repl) # Add any special replacements
         WeaponsList.append(self) # Add to the list
 
-AegisSword = Weapon(1, [ BroadswordData, WhipswordsData, BigBangEdgeData, DualScythesData, CatalystScimitarData])
-CatScim = Weapon(2, [AegisSwordData, BroadswordData, WhipswordsData, BigBangEdgeData, DualScythesData])
+AegisSword = Weapon(1, [WhipswordsData, BigBangEdgeData, DualScythesData, CatalystScimitarData])
+CatScim = Weapon(2, [AegisSwordData, WhipswordsData, BigBangEdgeData, DualScythesData])
 TwinRings = Weapon(3)
 DrillShield = Weapon(4, [ VariableSaberData, MechArmsData],[],True )
 MechArms = Weapon(5, [DrillShieldData, VariableSaberData],[] ,True)
@@ -59,45 +61,69 @@ ShieldHammer = Weapon(13)
 ChromaKatana = Weapon(14)
 Bitball = Weapon(15)
 KnuckleClaws = Weapon(16)
+Monado = Weapon(33)
+Knives = Weapon(34)
+DualSwords = Weapon(35)
+Uchigatana = Weapon(36)
 Broadsword = Weapon(17, [AegisSwordData, WhipswordsData, BigBangEdgeData, DualScythesData, CatalystScimitarData], [{"id": 12, "DefWeapon": 5661}])
 
 
 def WepRando():
      with open("./_internal/JsonOutputs/common/CHR_Bl.json", 'r+', encoding='utf-8') as BladeFile:
-        BlData = json.load(BladeFile)
-        InvalidBladeIds = [1008, 1013, 1042,1103, 1081, 1078,1079,1080, 1077] + Helper.InclRange(1082, 1101) + Helper.InclRange(1113, 1132) # Roc and all the utsuros
-        
-        # Might have to give vandahm a custom blade with scythes instead of roc because if i want to randomize roc it will break vandahm
+        with open("./_internal/JsonOutputs/common/BTL_Arts_Bl.json", 'r+', encoding='utf-8') as BladeArtsFile:
+            with open("./_internal/JsonOutputs/common/BTL_Arts_BlSp.json", 'r+', encoding='utf-8') as BladeSpArtsFile:
+                BlData = json.load(BladeFile)
+                BlArtData = json.load(BladeArtsFile)
+                BlSpArtData = json.load(BladeSpArtsFile)
+                InvalidBladeIds = [1008, 1013, 1042,1103, 1081, 1078,1079,1080, 1077] + Helper.InclRange(1082, 1101) + Helper.InclRange(1113, 1132) # Roc and all the utsuros
+                
+                # Might have to give vandahm a custom blade with scythes instead of roc because if i want to randomize roc it will break vandahm
 
-        for Blade in BlData["rows"]:
+                for Blade in BlData["rows"]:
 
-            curID = Blade["$id"]
-            if curID in InvalidBladeIds: # Skip the invalid blades
-                continue
+                    curID = Blade["$id"]
+                    if curID in InvalidBladeIds: # Skip the invalid blades
+                        continue
                     
-            
-            for Wep in WeaponsList:
-                if Blade["WeaponType"] == Wep.ID:
-                    replWep = random.choice(Wep.Replacements)
-                    Blade["WeaponType"] = replWep["id"]
-                    Blade["DefWeapon"] = replWep["DefWeapon"]
-                    break
-            
+                    for Wep in WeaponsList:
+                        if Blade["WeaponType"] == Wep.ID:
+                            replWep = random.choice(Wep.Replacements)
+                            Blade["WeaponType"] = replWep["id"]
+                            Blade["DefWeapon"] = replWep["DefWeapon"]
+                            
+                            Specials = [Blade["BArts1"], Blade["BArts2"], Blade["BArts3"]]
+                            ExSpecials = [Blade["BartsEx"], Blade["BartsEx2"]]
+                            
+                            # Changes lv1-4 specials weapons
+                            # for spID in Specials:
+                            #     for sp in BlArtData["rows"]:
+                            #         if sp["$id"] == spID:
+                            #             sp["WpnType"] = replWep["id"]
+                            #             break              
+                            # for spID in ExSpecials:
+                            #     for sp in BlSpArtData["rows"]:
+                            #         if sp["$id"] == spID:
+                            #             sp["WpnType"] = replWep["id"]
+                            #             break
+                            break
                     
-                    
+                            
+                FixArtLevels()  
+                FixBladeWepModels()
+                
+                BladeSpArtsFile.seek(0)
+                BladeSpArtsFile.truncate()
+                json.dump(BlSpArtData, BladeSpArtsFile, indent=2, ensure_ascii=False)
+            BladeArtsFile.seek(0)
+            BladeArtsFile.truncate()
+            json.dump(BlArtData, BladeArtsFile, indent=2, ensure_ascii=False)        
         BladeFile.seek(0)
         BladeFile.truncate()
         json.dump(BlData, BladeFile, indent=2, ensure_ascii=False)
 
 
 def FixArtLevels():
-    pass
-    # Make art levels on BTL_DRArts all 0 so we start with the arts
+    JSONParser.ChangeJSONLine(["common/BTL_Arts_Dr.json"],[], Helper.StartsWith("ReleaseLv", 1,5), [0], replaceAll=True)
     
 def FixBladeWepModels():
-    pass
-    # Make ONLYWPN on chr bl false for the ones we want to change 
-    
-def FixSpecialWeapons():
-    pass
-    # Make sure blades specials use that weapon in one of their parameters because it is need to work
+    JSONParser.ChangeJSONLine(["common/CHR_Bl.json"],[1], ["OnlyWpn"], [0])
