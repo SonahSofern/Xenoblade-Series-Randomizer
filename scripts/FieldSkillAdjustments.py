48a90e7190c7c3804d8428d7b1cc7eae30037dc7
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2025-02-01 15:46:59 -0900

    Update FieldSkillAdjustments.py

--- scripts/FieldSkillAdjustments.py
+++ scripts/FieldSkillAdjustments.py
@@ -24,7 +24,7 @@ def RemoveFieldSkills(Options):
         ]
         npcPopIds = [
             8006,           # Green Barrel
-            5268            # Pyra's Cooking
+            5267            # Pyra's Cooking
         ]    
         jumpGimiickIds = [
             34, 33,38,      # Cliffs of Morytha
