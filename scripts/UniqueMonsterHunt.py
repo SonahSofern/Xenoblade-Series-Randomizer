3f3e201ef2c8452ae05d251288ebe78bfabaf7cb
ofc2000alex <ofc2000alex@gmail.com>
2025-02-15 20:49:54 -0600

    Fixed several UM Hunt issues that I noticed, increased cost of quite a few items for um hunt

--- scripts/UniqueMonsterHunt.py
+++ scripts/UniqueMonsterHunt.py
@@ -1,7 +1,7 @@
 import json, random, Helper, IDs, EnemyRandoLogic, RaceMode, math, Options, time
 from Enhancements import *
 from collections import OrderedDict
-from BladeRandomization import Original2Replacement
+from BladeRandomization import Replacement2Original
 
 #Keeping these 3 separate from the already existing IDs in EnemyRandoLogic in case I want to do some balancing or something
 AllUniqueMonsterDefaultIDs = [611, 612, 705, 706, 707, 708, 709, 710, 711, 712, 713, 715, 736, 738, 808, 809, 810, 811, 812, 814, 815, 816, 817, 819, 890, 891, 892, 893, 894, 895, 896, 898, 899, 926, 929, 953, 954, 955, 957, 958, 1019, 1020, 1023, 1025, 1026, 1101, 1102, 1104, 1106, 1108, 1109, 1111, 1112, 1113, 1114, 1115, 1131, 1132, 1134, 1155, 1156, 1157, 1181, 1182, 1183, 1184, 1185, 1186, 1187, 1188, 1255, 1256, 1258, 1260, 1261, 1262, 1264, 1265, 1563, 1564, 1566, 1567, 1657, 1658, 1659, 1660, 1661, 1662, 1663, 1664, 1665, 1666, 1667, 1670, 1774]
@@ -20,15 +20,15 @@ PartyMembersAddScripts = {"Tora": ["chapt02", 7], "Nia": ["chapt02", 9], "Morag"
 
 # "Area Name": [Valid Unique Enemies]
 OriginalAreaEnemies = {
-    "Gormott": [184, 185, 186, 187, 189, 190, 191, 193, 195, 196, 197, 198, 256, 266, 303, 304, 329, 332, 341, 342, 345, 346, 347, 348, 349, 350, 352, 487, 488, 489, 490, 491, 492, 546, 547, 548, 559, 572, 598, 600, 601, 602, 603, 604, 607, 608, 609, 610, 611, 635, 636, 637, 638, 639, 641, 642, 643, 644, 645, 646, 647, 648, 649, 650, 651, 652, 653, 654, 655, 656, 657, 658, 659, 660, 661, 662, 663, 664, 665, 666, 667, 668, 669, 670, 671, 672, 673, 674, 675, 676, 677, 678, 679, 680, 681, 682, 684, 685, 686, 687, 688, 689, 690, 691, 692, 693, 694, 695, 696, 697, 699, 701, 703, 705, 706, 707, 708, 709, 710, 711, 712, 713, 714, 715, 723, 729, 730, 731, 732, 733, 734, 735, 736, 738, 1320, 1321, 1326, 1329, 1386, 1387],
+    "Gormott": [184, 185, 186, 187, 189, 190, 191, 193, 195, 196, 197, 198, 266, 303, 304, 329, 332, 341, 342, 345, 346, 347, 348, 349, 350, 352, 487, 488, 489, 490, 491, 492, 546, 547, 548, 559, 572, 598, 600, 601, 602, 603, 604, 607, 608, 609, 610, 611, 635, 636, 637, 638, 639, 641, 642, 643, 644, 645, 646, 647, 648, 649, 650, 651, 652, 653, 654, 655, 656, 657, 658, 659, 660, 661, 662, 663, 664, 665, 666, 667, 668, 669, 670, 671, 672, 673, 674, 675, 676, 677, 678, 679, 680, 681, 682, 684, 685, 686, 687, 688, 689, 690, 691, 692, 693, 694, 695, 696, 697, 699, 701, 703, 705, 706, 707, 708, 709, 710, 711, 712, 713, 714, 715, 723, 729, 730, 731, 732, 733, 734, 735, 736, 738, 1320, 1321, 1326, 1329, 1386, 1387],
     "Uraya": [199, 201, 202, 203, 204, 206, 208, 210, 212, 214, 267, 268, 305, 307, 308, 309, 310, 356, 365, 367, 369, 372, 373, 374, 375, 407, 409, 411, 451, 479, 481, 483, 485, 496, 503, 506, 508, 510, 512, 536, 538, 567, 577, 578, 579, 581, 583, 588, 591, 593, 739, 740, 741, 742, 743, 744, 745, 746, 747, 748, 749, 750, 751, 752, 753, 754, 755, 756, 757, 759, 760, 761, 762, 763, 764, 765, 766, 767, 768, 769, 770, 771, 772, 773, 774, 775, 776, 777, 778, 779, 780, 781, 782, 783, 784, 785, 786, 787, 788, 789, 790, 791, 792, 793, 794, 795, 796, 797, 798, 800, 802, 804, 806, 808, 809, 810, 811, 812, 814, 815, 816, 817, 819, 1674],
     "Mor Ardain": [216, 217, 219, 220, 221, 222, 223, 225, 227, 269, 270, 271, 313, 315, 383, 384, 385, 386, 389, 390, 391, 392, 393, 394, 395, 396, 399, 401, 403, 404, 405, 406, 454, 493, 495, 517, 519, 521, 523, 525, 532, 533, 534, 535, 540, 542, 544, 549, 550, 551, 555, 571, 821, 822, 823, 824, 825, 826, 827, 828, 829, 830, 831, 832, 833, 834, 835, 836, 837, 838, 839, 840, 841, 842, 843, 844, 845, 847, 848, 849, 850, 851, 852, 853, 854, 855, 856, 857, 858, 859, 860, 861, 862, 863, 864, 865, 866, 867, 868, 869, 870, 871, 872, 873, 874, 875, 876, 877, 878, 879, 880, 881, 882, 884, 886, 888, 890, 891, 892, 893, 894, 895, 896, 898, 899, 906, 908, 909, 910, 911, 912, 913, 914, 915, 916, 917, 918, 919, 920, 922, 924, 926, 928, 929, 1343, 1388, 1418, 1419, 1675, 1676, 1677, 1678, 1679, 1680, 1681],
     "Leftheria": [229, 318, 319, 414, 415, 418, 445, 446, 447, 448, 474, 476, 498, 503, 553, 1196, 1197, 1198, 1199, 1200, 1201, 1202, 1203, 1204, 1205, 1206, 1207, 1208, 1209, 1210, 1211, 1212, 1213, 1214, 1215, 1216, 1217, 1218, 1219, 1220, 1221, 1222, 1223, 1224, 1225, 1226, 1227, 1228, 1229, 1230, 1231, 1232, 1233, 1234, 1235, 1236, 1237, 1238, 1239, 1240, 1241, 1242, 1243, 1244, 1245, 1246, 1247, 1248, 1249, 1250, 1251, 1254, 1255, 1256, 1258, 1260, 1261, 1262, 1264, 1265, 1344, 1345, 1395, 1397, 1398, 1399, 1415, 1416, 1417, 1684, 1686, 1687, 1688, 1689, 1690],
     "Temperantia": [231, 232, 234, 376, 475, 477, 500, 501, 504, 552, 560, 567, 569, 964, 965, 966, 967, 968, 969, 970, 971, 972, 973, 974, 975, 976, 977, 978, 979, 980, 981, 982, 983, 984, 985, 986, 987, 988, 989, 990, 991, 992, 993, 994, 995, 996, 997, 998, 999, 1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1015, 1016, 1017, 1019, 1020, 1022, 1023, 1025, 1026, 1027, 1342, 1389, 1391, 1393, 1408, 1410, 1412, 1413, 1414],
     "Tantal": [237, 238, 239, 240, 241, 323, 436, 437, 455, 456, 457, 458, 459, 461, 462, 463, 513, 515, 566, 576, 1035, 1036, 1037, 1038, 1039, 1040, 1041, 1042, 1043, 1044, 1045, 1046, 1047, 1048, 1049, 1050, 1051, 1052, 1053, 1054, 1055, 1056, 1057, 1058, 1059, 1060, 1061, 1062, 1063, 1064, 1065, 1066, 1067, 1068, 1069, 1070, 1071, 1072, 1073, 1074, 1075, 1076, 1077, 1078, 1079, 1080, 1081, 1082, 1083, 1084, 1085, 1086, 1087, 1088, 1089, 1090, 1091, 1092, 1093, 1094, 1095, 1096, 1097, 1098, 1099, 1100, 1101, 1102, 1104, 1106, 1108, 1109, 1110, 1111, 1112, 1113, 1114, 1115, 1404, 1405, 1406, 1425, 1682, 1683, 1888],
     "Spirit Crucible": [242, 359, 497, 503, 570, 573, 930, 931, 932, 933, 934, 935, 936, 938, 939, 940, 941, 942, 943, 944, 945, 946, 947, 948, 949, 950, 951, 952, 953, 954, 955, 957, 958],
-    "Cliffs of Morytha": [243, 244, 245, 260, 262, 1116, 1117, 1118, 1119, 1120, 1121, 1122, 1123, 1124, 1125, 1126, 1127, 1128, 1131, 1132, 1134, 1135, 1137, 1400, 1402],
-    "Land of Morytha": [248, 249, 250, 258, 274, 324, 351, 565, 1145, 1146, 1147, 1148, 1149, 1150, 1151, 1152, 1153, 1154, 1155, 1156, 1157, 1423, 1424],
+    "Cliffs of Morytha": [243, 244, 245, 1116, 1117, 1118, 1119, 1120, 1121, 1122, 1123, 1124, 1125, 1126, 1127, 1128, 1131, 1132, 1134, 1135, 1137, 1400, 1402],
+    "Land of Morytha": [248, 249, 250, 274, 324, 351, 565, 1145, 1146, 1147, 1148, 1149, 1150, 1151, 1152, 1153, 1154, 1155, 1156, 1157, 1423, 1424],
     "World Tree": [251, 252, 253, 254, 325, 326, 557, 558, 564, 1158, 1159, 1160, 1161, 1162, 1163, 1164, 1165, 1166, 1167, 1168, 1170, 1171, 1172, 1173, 1174, 1175, 1176, 1177, 1178, 1181, 1182, 1183, 1184, 1185, 1186, 1187, 1188, 1189, 1422]
 }
 
@@ -451,7 +451,7 @@ def FieldQuestTaskLogSetup(EnemySets): # Adds the task logs for the field quests
                 for row in data["rows"]:
                     if row["$id"] == EnemySets[i][j]:
                         CurrEnemySetNameIDs.append(row["Name"])
-                        row["Lv"] = 5 + 12*i # Sets level of enemy equal to 5 min, then for each set after, the level goes up by 11 more
+                        row["Lv"] = 5 + 12*i # Sets level of enemy equal to 5 min, then for each set after, the level goes up by 12 more
                         LuckyDrop = random.randint(0, 99)
                         if LuckyDrop <= 74:
                             break
@@ -530,6 +530,7 @@ def CustomEnemyRando(ChosenAreaOrder): # Custom shuffling of enemies
     SuperbossMapsFull = []
     AllOriginalAreaEnemies = []
     AllNewAreaEnemies = []
+    CopyofAllNormalEnemyDefaultIDs = AllNormalEnemyDefaultIDs.copy()
     ShuffledUniqueEnemyIDs = AllUniqueMonsterDefaultIDs.copy()
     ShuffledSuperBossIDs = AllSuperBossDefaultIDs.copy()
     random.shuffle(ShuffledSuperBossIDs)
@@ -550,10 +551,10 @@ def CustomEnemyRando(ChosenAreaOrder): # Custom shuffling of enemies
             if ExtraSuperbosses:
                 NewAreaEnemies.append(ChosenSuperBoss) # add the superboss to the pool
             while len(NewAreaEnemies) < len(OriginalAreaEnemies[ChosenAreaOrder[k]]): # while the new enemy pool is less than the original, we need to add more to make them equal
-                ChosenNormalEnemy = random.choice(AllNormalEnemyDefaultIDs)
+                ChosenNormalEnemy = random.choice(CopyofAllNormalEnemyDefaultIDs)
                 if ChosenNormalEnemy not in NewAreaEnemies:
                     NewAreaEnemies.append(ChosenNormalEnemy)
-                    AllNormalEnemyDefaultIDs.remove(ChosenNormalEnemy)
+                    CopyofAllNormalEnemyDefaultIDs.remove(ChosenNormalEnemy)
             random.shuffle(NewAreaEnemies) # now mix up the enemies
             Old2NewEnemy = {OriginalAreaEnemies[ChosenAreaOrder[k]][i]: NewAreaEnemies[i] for i in range(len(OriginalAreaEnemies[ChosenAreaOrder[k]]))} # dictionary showing how old enemy should get replaced by new enemy
             for row in data["rows"]: # row ~550 loops
@@ -588,6 +589,7 @@ def CustomEnemyRando(ChosenAreaOrder): # Custom shuffling of enemies
         AllAreaMonsters.append(CurrentAreaMonsters)
         AllOriginalAreaEnemies.extend(OriginalAreaEnemies[ChosenAreaOrder[k]])
         AllNewAreaEnemies.extend(NewAreaEnemies)
+        CopyofAllNormalEnemyDefaultIDs = [x for x in CopyofAllNormalEnemyDefaultIDs if x not in NewAreaEnemies]
     EnemyRandoLogic.FlyingEnemyFix(AllOriginalAreaEnemies, AllNewAreaEnemies)
     EnemyRandoLogic.SwimmingEnemyFix(AllOriginalAreaEnemies, AllNewAreaEnemies)
     EnemyRandoLogic.FishFix()
@@ -1007,7 +1009,7 @@ def IdentifyDLCBladeCrystals(CrystalList):
     if Options.BladesOption.GetState():
         RandomizedBladeIDs = []
         for originalblade in DLCBladeIDs:
-            RandomizedBladeIDs.append(Original2Replacement[originalblade])
+            RandomizedBladeIDs.append(Replacement2Original[originalblade])
         DLCBladeIDs = RandomizedBladeIDs
         #print(DLCBladeIDs)
     DLCBladeCrystalList = []
@@ -1725,20 +1727,20 @@ def CustomShopSetup(): # Sets up the custom shops with loot
     TokenExchangeRewards = []
     for i in range(0, 10):
         TokenExchangeRewards.append([random.randint(2 + 4*i, 4 + 5*i)])
-    CoreCrystalCostDistribution = [4, 8, 12, 16, 4, 8, 12, 4, 8, 12, 20, 30, 40, 130, 145, 160]
-    ManualCostDistribution = [3, 6, 9, 20, 35, 9, 17, 33]
-    ChipShopCostDistribution = Helper.ExtendListtoLength([1],16,"inputlist[i-1]+9")
+    CoreCrystalCostDistribution = [5, 10, 15, 20, 5, 10, 15, 10, 15, 20, 30, 40, 50, 130, 145, 160]
+    ManualCostDistribution = [3, 6, 9, 35, 50, 9, 17, 33]
+    ChipShopCostDistribution = Helper.ExtendListtoLength([1],16,"inputlist[i-1]+8")
     
     CommonAuxCoreCosts = [3, 6, 9, 12, 3, 6, 9]
-    RareAuxCoreCosts = [6, 12, 6, 12, 10]
-    LegendaryAuxCoreCosts = [20, 20, 30, 30]
+    RareAuxCoreCosts = [10, 20, 10, 20, 15]
+    LegendaryAuxCoreCosts = [60, 60, 70, 70]
     AuxCoreShopCostDistribution = CommonAuxCoreCosts + RareAuxCoreCosts + LegendaryAuxCoreCosts
 
     PouchItemShopCostDistribution = [5,5,5,5,10,5,5,5,15,15,15,10,10,15,10,5]
     
     CommonAccessoryCosts = [4, 8, 12, 16, 4, 8, 12]
-    RareAccessoryCosts = [15, 20, 15, 20, 25]
-    LegendaryAccessoryCosts = [30, 30, 50, 50]
+    RareAccessoryCosts = [20, 30, 20, 30, 35]
+    LegendaryAccessoryCosts = [60, 60, 70, 70]
     AccessoryShopCostDistribution = CommonAccessoryCosts + RareAccessoryCosts + LegendaryAccessoryCosts
 
     PoppiswapShopCosts = [10, 20, 30, 40, 50, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44]
