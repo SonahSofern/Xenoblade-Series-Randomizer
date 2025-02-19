5c856151d111ed469d33d687c18b8bd8f06e9304
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2025-02-19 12:25:34 -0900

    More fixing

--- XC2/Scales.py
+++ XC2/Scales.py
@@ -1,14 +1,48 @@
+import json, random, Options
+from scripts import Helper
+
+
 def BladeScales():
-    pass
+    BladeScales = [20,30,40,50,60,70,80,90,110,120,130,140,160,180,200,400,600]
+
+    with open(f"./XC2/_internal/JsonOutputs/common/CHR_Bl.json", 'r+', encoding='utf-8') as bladeFile:
+        bladeData = json.load(bladeFile)
+        odds = Options.BladeSizeOption.GetOdds()
+        for blade in bladeData["rows"]:
+            if not Helper.OddsCheck(odds): # Check spinbox
+                continue
+            scale = random.choice(BladeScales)  # Make our selection
+            blade["Scale"] =  scale
+            blade["WpnScale"] = scale
+        bladeFile.seek(0)
+        bladeFile.truncate()
+        json.dump(bladeData, bladeFile, indent=2, ensure_ascii=False)
 
 def NPCScales():
-    pass
+    NPCScales = [20,30,40,50,60,70,80,90,110,120,130,140,160,180,200,400,600]
+    
+    with open(f"./XC2/_internal/JsonOutputs/common/RSC_NpcList.json", 'r+', encoding='utf-8') as npcFile:
+        npcData = json.load(npcFile)
+        odds = Options.NPCSizeOption.GetOdds()
+        for npc in npcData["rows"]:
+            if not Helper.OddsCheck(odds): # Check spinbox
+                continue
+            npc["Scale"] = random.choice(NPCScales)  # Make our selection
+        npcFile.seek(0)
+        npcFile.truncate()
+        json.dump(npcData, npcFile, indent=2, ensure_ascii=False)
+
 
 def EnemyScales():
-    pass
-
-
-
-# BladeSizeOption = Option("Blade Size", Funny, "Randomizes the size of Blades", [lambda: JSONParser.ChangeJSONFile(["common/CHR_Bl.json"], ["Scale", "WpnScale"], [], BladeScales)], _hasSpinBox = True)
-# NPCSizeOption = Option("NPC Size", Funny, "Randomizes the size of NPCs", [lambda: JSONParser.ChangeJSONFile(["common/RSC_NpcList.json"], ["Scale"],[], NPCScales)], _hasSpinBox = True)
-# EnemySizeOption = Option("Enemy Size", Funny, "Randomizes the size of enemies", [lambda: JSONParser.ChangeJSONFile(["common/CHR_EnArrange.json"], ["Scale"], [], EnemyScales)], _hasSpinBox = True)
+    EnemyScales= [10,20,30,40,50,60,70,80,90,110,120,130,140,160,180,200,400,600,1000,3000]
+    
+    with open(f"./XC2/_internal/JsonOutputs/common/CHR_EnArrange.json", 'r+', encoding='utf-8') as enFile:
+        enData = json.load(enFile)
+        odds = Options.EnemySizeOption.GetOdds()
+        for en in enData["rows"]:
+            if not Helper.OddsCheck(odds): # Check spinbox
+                continue
+            en["Scale"] = random.choice(EnemyScales)  # Make our selection
+        enFile.seek(0)
+        enFile.truncate()
+        json.dump(enData, enFile, indent=2, ensure_ascii=False)
