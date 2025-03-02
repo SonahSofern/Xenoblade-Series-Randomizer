1e7802c69ffc3a688218af28d5e6c0345af82f65
SonahSofern <94713741+SonahSofern@users.noreply.github.com>
2025-03-01 20:20:58 -0900

    got enemies workin

--- XCDE/XCDE_Scripts/Enemies.py
+++ XCDE/XCDE_Scripts/Enemies.py
@@ -1,20 +1,11 @@
 
 import json, random, Options, IDs
 
-#Enelist is needed in conjuction with poplist not sure how they are referencing each other yet though
-
-class Enemy:
-    def __init__(self, _id, _name, _resource, _c_name_id, _mnu_vision_face):
-        self.id = _id
-        self.name = _name
-        self.resource = _resource
-        self.cname = _c_name_id
-        self.mnuFace = _mnu_vision_face 
-
-
+        
 def Enemies():
     BadEnemyIds = []
 
+    # Create Bad Enemy List ID
     with open(f"./XCDE/_internal/JsonOutputs/bdat_common/BTL_enelist.json", 'r+', encoding='utf-8') as eneFile:
         eneData = json.load(eneFile)
         
@@ -27,64 +18,55 @@ def Enemies():
         eneFile.truncate()
         json.dump(eneData, eneFile, indent=2, ensure_ascii=False)
 
-    
+    # Create our Enemies List
     EnemyList = []
     for file in IDs.areaFileListNumbers:
-        try:
-            with open(f"./XCDE/_internal/JsonOutputs/bdat_ma{file}/poplist{file}.json", 'r+', encoding='utf-8') as eneFile:
+        try: # Create our list of enemies
+            with open(f"./XCDE/_internal/JsonOutputs/bdat_ma{file}/BTL_enelist{file}.json", 'r+', encoding='utf-8') as eneFile:
                 eneData = json.load(eneFile)
                 
                 for enemy in eneData["rows"]:
-                    for i in range(1,6):
-                        if enemy[f"ene{i}ID"] == 0:
-                            continue
-                        if enemy[f"ene{i}ID"] in BadEnemyIds:
-                            continue
-                        EnemyList.append(enemy[f"ene{i}ID"])
+                    if enemy["$id"] in BadEnemyIds:
+                        continue
+                    EnemyList.append(enemy)
                             
-                
                 eneFile.seek(0)
                 eneFile.truncate()
                 json.dump(eneData, eneFile, indent=2, ensure_ascii=False)
         except:
             continue
+    # Randomize the enemy
     for file in IDs.areaFileListNumbers:
         try:
-            with open(f"./XCDE/_internal/JsonOutputs/bdat_ma{file}/poplist{file}.json", 'r+', encoding='utf-8') as eneFile:
-                eneData = json.load(eneFile)
-                
-                for enemy in eneData["rows"]:
-                    for i in range(1,6):
-                        if enemy[f"ene{i}ID"] == 9:
-                            chosen = random.choice(EnemyList)
-                            enemy[f"ene{i}ID"] = 62
-                            EnemyList.remove(chosen)
-                            
-                
+            with open(f"./XCDE/_internal/JsonOutputs/bdat_ma{file}/BTL_enelist{file}.json", 'r+', encoding='utf-8') as eneFile:
+                with open(f"./XCDE/_internal/JsonOutputs/bdat_ma{file}/poplist{file}.json", 'r+', encoding='utf-8') as popFile:
+                    eneData = json.load(eneFile)
+                    popData= json.load(popFile)
+                    
+                    for enemy in eneData["rows"]:
+                        
+                        if enemy["$id"] in BadEnemyIds:
+                            continue
+                        
+                        chosen = random.choice(EnemyList)
+                        
+                        for popEnemy in popData["rows"]:
+                            for i in range(1,5):
+                                if popEnemy[f"ene{i}ID"] == enemy["$id"]:
+                                    popEnemy[f"ene{i}ID"] = chosen["$id"]
+                                    
+                        EnemyList.remove(chosen)
+                        for key in ["$id", "move_speed", "run_speed", "named", "frame", "size", "scale", "family", "elem_phx", "elem_eth", "Lv_up_hp", "Lv_up_str", "anti_state", "resi_state", "elem_tol", "elem_tol_dir", "down_grd", "faint_grd", "front_angle", "avoid", "delay", "hit_range_far", "dbl_atk", "cnt_atk", "detects", "assist", "search_range", "search_angle", "chest_height", "spike_elem", "spike_type", "spike_range", "spike_dmg", "spike_state", "spike_state_val", "atk1", "atk2", "atk3", "arts1", "arts2", "arts3", "arts4", "arts5", "arts6", "arts7", "arts8"]:
+                            enemy[key] = chosen[key]
+
+                        
+                        
+                    popFile.seek(0)
+                    popFile.truncate()
+                    json.dump(popData, popFile, indent=2, ensure_ascii=False)         
                 eneFile.seek(0)
                 eneFile.truncate()
                 json.dump(eneData, eneFile, indent=2, ensure_ascii=False)
         except:
             continue
     
-    
-    # for file in IDs.areaFileListNumbers:
-    #     with open(f"./XCDE/_internal/JsonOutputs/bdat_ma{file}/BTL_enelist{file}.json", 'r+', encoding='utf-8') as eneFile:
-    #         eneData = json.load(eneFile)
-    #         EnemyList:list[Enemy] = []
-            
-    #         for enemy in eneData["rows"]: # Create our classes of enemies
-    #             curEnemy = Enemy(enemy["$id"], enemy["name"], enemy["resource"], enemy["c_name_id"], enemy["mnu_vision_face"])
-    #             EnemyList.append(curEnemy)
-            
-    #         for enemy in eneData["rows"]:
-    #             chosen = random.choice(EnemyList)
-    #             EnemyList.remove(chosen)
-    #             enemy["name"] = chosen.name
-    #             enemy["resource"] = chosen.resource
-    #             enemy["c_name_id"] = chosen.cname
-    #             enemy["mnu_vision_face"] = chosen.mnuFace
-            
-    #         eneFile.seek(0)
-    #         eneFile.truncate()
-    #         json.dump(eneData, eneFile, indent=2, ensure_ascii=False)
