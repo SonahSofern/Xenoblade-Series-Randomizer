9990b6de82ce3cd9fb44b56972980cf99bcba39c
ofc2000alex <ofc2000alex@gmail.com>
2025-03-08 16:40:49 -0600

    Removed Cressidus' Trespass from the list of Valid Yellow Blade Skill Tree Skills, due to reports of crashing the game.

--- XC2/XC2_Scripts/IDs.py
+++ XC2/XC2_Scripts/IDs.py
@@ -23,7 +23,7 @@ VanillaEnhancements = Helper.InclRange(1,3894)
 DriverSkillTrees = Helper.InclRange(1,270)
 HitReactions = Helper.InclRange(0,16)
 ButtonCombos = Helper.InclRange(1,5)
-BladeBattleSkills = list(set(Helper.InclRange(1,270)) - set([268, 267,266,265,144,142,143, 8, 9]))   
+BladeBattleSkills = list(set(Helper.InclRange(1,270)) - set([268, 267,266,265,144,142,143,141,8,9]))   
 
 MajorAreaIds = ["01","02","03","04","05","07","08","10","11","13","15","17","18","20","21","30","40","41","42","49","50","90"]
 
