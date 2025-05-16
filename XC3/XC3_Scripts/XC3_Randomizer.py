c50afafa75a8566cd7ef0cce151c6e1deb9bba0d
Ben <94713741+SonahSofern@users.noreply.github.com>
2025-05-15 21:19:16 -0900

    added the hashes

--- XC3/XC3_Scripts/XC3_Randomizer.py
+++ XC3/XC3_Scripts/XC3_Randomizer.py
@@ -162,7 +162,7 @@ randoProgressDisplay = ttk.Label(text="", anchor="e", padding=2, style="Borderle
 areaFiles = []
 
 # Randomize Button
-RandomizeButton = ttk.Button(text='Randomize', command=(lambda: GUISettings.Randomize(RandomizeButton,fileEntryVar, randoProgressDisplay, bdat_path, permalinkVar, randoSeedEntry, JsonOutput, outputDirVar, Interactables.OptionList, ["mnu"] )))
+RandomizeButton = ttk.Button(text='Randomize', command=(lambda: GUISettings.Randomize(RandomizeButton,fileEntryVar, randoProgressDisplay, bdat_path, permalinkVar, randoSeedEntry, JsonOutput, outputDirVar, Interactables.OptionList, ["mnu", "des", "btl", "evt", "fld", "map", "prg", "qst", "sys"] , ["game/autotalk", "game/battle", "game/field", "game/menu", "game/quest", "game/system"], textFolderName="gb")))
 RandomizeButton.place(relx=0.5, rely=1, y= -10, anchor="s")
 RandomizeButton.config(padding=5)
 
