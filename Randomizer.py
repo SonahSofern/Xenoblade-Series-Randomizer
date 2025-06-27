d321be7b68a6258fc89bbb486fba7d61fa8432b7
Ben <94713741+SonahSofern@users.noreply.github.com>
2025-06-26 15:40:53 -0900

    cleaned up grouping

--- Randomizer.py
+++ Randomizer.py
@@ -5,6 +5,10 @@ from tkinter import ttk
 from tkinter.font import Font
 root = Tk()
 from scripts import GUISettings, XCRandomizer
+import XCDE.XCDE_Scripts.XCDE_Settings as XCDE
+import XC2.XC2_Scripts.XC2_Settings as XC2
+import XC3.XC3_Scripts.XC3_Settings as XC3
+import XCXDE.XCXDE_Scripts.XCXDE_Settings as XCXDE
 
 GUISettings.RootsForStyling.append(root)
 
@@ -29,17 +33,10 @@ else:
 icon = PhotoImage(file=icon_path)
 root.iconphoto(True, icon)
 
-from XCDE.XCDE_Scripts.XCDE_Settings import *
-XCRandomizer.CreateMainWindow(root, MainWindow, "XCDE", Version, "Xenoblade Chronicles DE Randomizer", Options.Tabs, extraCommands, mainFolderNames, subFolderNames, SeedNames.Nouns, SeedNames.Verbs, backgroundImages=backgrounds, extraFiles=[TitlescreenSplash])
-
-from XC2.XC2_Scripts.XC2_Settings import *
-XCRandomizer.CreateMainWindow(root, MainWindow, "XC2", Version, "Xenoblade Chronicles 2 Randomizer", Options.Tabs, extraCommands, mainFolderNames, subFolderNames, SeedNames.Nouns, SeedNames.Verbs, backgroundImages=backgrounds, extraFiles=[TitlescreenSplash])
-
-from XC3.XC3_Scripts.XC3_Settings import *
-XCRandomizer.CreateMainWindow(root, MainWindow, "XC3", Version, "Xenoblade Chronicles 3 Randomizer", Options.Tabs, extraCommands, mainFolderNames, subFolderNames, SeedNames.Nouns, SeedNames.Verbs, textFolderName,extraArgs=extraArgs, backgroundImages=backgrounds)
-
-from XCXDE.XCXDE_Scripts.XCXDE_Settings import *
-XCRandomizer.CreateMainWindow(root, MainWindow, "XCXDE", Version, "Xenoblade Chronicles X DE Randomizer", Options.Tabs, extraCommands, mainFolderNames, subFolderNames, SeedNames.Nouns, SeedNames.Verbs, backgroundImages=backgrounds)
+XCRandomizer.CreateMainWindow(root, MainWindow, "XCDE", XCDE.Version, "Xenoblade Chronicles DE Randomizer", XCDE.Options.Tabs, XCDE.extraCommands, XCDE.mainFolderNames, XCDE.subFolderNames, XCDE.SeedNames.Nouns, XCDE.SeedNames.Verbs, backgroundImages=XCDE.backgrounds, extraFiles=[XCDE.TitlescreenSplash])
+XCRandomizer.CreateMainWindow(root, MainWindow, "XC2", XC2.Version, "Xenoblade Chronicles 2 Randomizer", XC2.Options.Tabs, XC2.extraCommands, XC2.mainFolderNames, XC2.subFolderNames, XC2.SeedNames.Nouns, XC2.SeedNames.Verbs, backgroundImages=XC2.backgrounds, extraFiles=[XC2.TitlescreenSplash])
+XCRandomizer.CreateMainWindow(root, MainWindow, "XC3", XC3.Version, "Xenoblade Chronicles 3 Randomizer", XC3.Options.Tabs, XC3.extraCommands, XC3.mainFolderNames, XC3.subFolderNames, XC3.SeedNames.Nouns, XC3.SeedNames.Verbs, XC3.textFolderName,extraArgs=XC3.extraArgs, backgroundImages=XC3.backgrounds)
+XCRandomizer.CreateMainWindow(root, MainWindow, "XCXDE", XCXDE.Version, "Xenoblade Chronicles X DE Randomizer", XCXDE.Options.Tabs, XCXDE.extraCommands, XCXDE.mainFolderNames, XCXDE.subFolderNames, XCXDE.SeedNames.Nouns, XCXDE.SeedNames.Verbs, backgroundImages=XCXDE.backgrounds)
 
 root.protocol("WM_DELETE_WINDOW", lambda: ([cmd() for cmd in XCRandomizer.saveCommands] and root.destroy()))
 
