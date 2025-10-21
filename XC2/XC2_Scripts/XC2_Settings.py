ef5199d9bde2f6260b17b1a1e6bbcd115114b873
Ben <94713741+SonahSofern@users.noreply.github.com>
2025-10-20 21:52:06 -0900

    Update XC2_Settings.py

--- XC2/XC2_Scripts/XC2_Settings.py
+++ XC2/XC2_Scripts/XC2_Settings.py
@@ -41,8 +41,6 @@ def XC2Help():
     descData.Text("Once the randomizer finishes, launch your game and you should see the version somewhere on the title screen.", anchor="w")
     descData.Text("If so, you're ready to start playing!", anchor="w")
     descData.Header("Common Issues")
-    descData.Tag("Game Modes")
-    descData.Text("Race Mode and Unique Monster Hunt do not work with all other settings, they will overwrite the incompatible choices.", anchor="w")
     descData.Tag("Mods")
     descData.Text("This mod is NOT compatible with other mods that edit the bdats. Ensure that this is the only active one for your game. (60fps and other visual mods should be okay)", anchor="w")
     return descData
