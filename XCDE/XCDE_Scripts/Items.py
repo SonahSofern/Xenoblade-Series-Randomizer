2bbc7dfced42929d59e90040d2526b9089967168
Ben <94713741+SonahSofern@users.noreply.github.com>
2025-11-26 17:21:17 -0900

    Almost there

--- XCDE/XCDE_Scripts/Items.py
+++ XCDE/XCDE_Scripts/Items.py
@@ -2,6 +2,9 @@ import json, random, copy
 from scripts import JSONParser, Helper, PopupDescriptions, Values
 from  XCDE.XCDE_Scripts import Options, IDs
 
+# Gems have no way to reconcile the rank right now they are all treated the same,  ideally when making it would just have a handler that multiplies the price found in skillist by the gems rank which is found in itemlist
+# Add mults to things that should feel rewarding (giants chest and collectapedia)
+
 def AllItemPricer():
     '''Adds prices to all items so they can be valued, basically just combines the tables since prices are held in seperate file'''
     AddPriceValuesToItemList("ITM_materiallist", IDs.MaterialIDs)
@@ -134,7 +137,7 @@ def Shops():
             for i in range(1,13):
                 wpnValTable.SelectValuedMember(shop, f"wpn{i}")
                 headValTable.SelectValuedMember(shop, f"head{i}")
-                headValTable.SelectValuedMember(shop, f"head{i}")
+                headValTable.SelectValuedMember(shop, f"body{i}")
                 armValTable.SelectValuedMember(shop, f"arm{i}")
                 waistValTable.SelectValuedMember(shop, f"waist{i}")
                 leggValTable.SelectValuedMember(shop, f"legg{i}")
@@ -151,20 +154,12 @@ def ShopsDesc():
 
 def EnemyDrops():
     valTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
-    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.MaterialIDs, Values.WeightOptionMethod(Options.GiantsChestOptions_Materials))
-    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.CollectableIDs, Values.WeightOptionMethod(Options.GiantsChestOptions_Collectables))
-    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.ArmorIDs, Values.WeightOptionMethod(Options.GiantsChestOptions_Armor))
-    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.WeaponIDs, Values.WeightOptionMethod(Options.GiantsChestOptions_Weapons))
-    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.GemIDs, Values.WeightOptionMethod(Options.GiantsChestOptions_Gems))
-    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.ArtBookIDs, Values.WeightOptionMethod(Options.GiantsChestOptions_ArtBooks))
-    
-    # Randomization
-    with open(f"XCDE/JsonOutputs/bdat_common/FLD_tboxlist.json", 'r+', encoding='utf-8') as tboxFile:
-        tboxData = json.load(tboxFile)
-        for col in tboxData["rows"]:
-            for i in range(1,5):
-                valTable.SelectValuedMember(col, f"item{i}ID", IDs.KeyItemIDs + IDs.ArtBookIDs)
-        JSONParser.CloseFile(tboxData, tboxFile)
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.MaterialIDs, Values.WeightOptionMethod(Options.EnemyDropOptions_Materials))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.CollectableIDs, Values.WeightOptionMethod(Options.EnemyDropOptions_Collectables))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.ArmorIDs, Values.WeightOptionMethod(Options.EnemyDropOptions_Armor))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.WeaponIDs, Values.WeightOptionMethod(Options.EnemyDropOptions_Weapons))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.GemIDs, Values.WeightOptionMethod(Options.EnemyDropOptions_Gems))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.ArtBookIDs, Values.WeightOptionMethod(Options.EnemyDropOptions_ArtBooks))
     
     nmlFiles = []
     rarFiles = []
@@ -174,78 +169,123 @@ def EnemyDrops():
         rarFiles.append(f"bdat_ma{file}/drop_rarlist{file}")
         sprFiles.append(f"bdat_ma{file}/drop_sprlist{file}")
         
-    sprKeys = ["wpn1", "wpn2", "wpn3", "wpn4"]
-    for i in range(1,9):
-        sprKeys.append(f"arts{i}")
-        sprKeys.append(f"uni_equip{i}")
-        sprKeys.append(f"uni_wpn{i}")
+    # Randomization
+    for file in nmlFiles:
+        with open(f"XCDE/JsonOutputs/{file}.json", 'r+', encoding='utf-8') as dropFile:
+            dropData = json.load(dropFile)
+            for drop in dropData["rows"]:
+                for i in range(1,3):
+                    valTable.SelectValuedMember(drop, f"materia{i}", IDs.KeyItemIDs)
+            JSONParser.CloseFile(dropData, dropFile)
     
-    ItemRandomization([col, mat, arm, wep, gem, cry, art, key], nmlFiles, odds,"XCDE",  ["materia1", "materia2"], keepType=keepType.GetState())
-    ItemRandomization([col, mat, arm, wep, gem, cry, art, key], rarFiles, odds,"XCDE",  ["crystal1", "crystal2", "wpn1", "wpn2", "wpn3", "wpn4", "equip1", "equip2", "equip3", "equip4"], keepType=keepType.GetState())
-    ItemRandomization([col, mat, arm, wep, gem, cry, art, key], sprFiles, odds,"XCDE",  sprKeys, keepType=keepType.GetState())
+    for file in rarFiles:
+        with open(f"XCDE/JsonOutputs/{file}.json", 'r+', encoding='utf-8') as dropFile:
+            dropData = json.load(dropFile)
+            for drop in dropData["rows"]:
+                for i in range(1,5):
+                    valTable.SelectValuedMember(drop, f"wpn{i}")
+                for i in range(1,5):
+                    valTable.SelectValuedMember(drop, f"equip{i}")
+            JSONParser.CloseFile(dropData, dropFile)
+            
+    for file in sprFiles:
+        with open(f"XCDE/JsonOutputs/{file}.json", 'r+', encoding='utf-8') as dropFile:
+            dropData = json.load(dropFile)
+            for drop in dropData["rows"]:
+                for i in range(1,5):
+                    valTable.SelectValuedMember(drop, f"wpn{i}")
+                for i in range(1,9):
+                    valTable.SelectValuedMember(drop, f"equip{i}")
+                    valTable.SelectValuedMember(drop, f"uni_wpn{i}")
+                    valTable.SelectValuedMember(drop, f"uni_equip{i}")
+            JSONParser.CloseFile(dropData, dropFile)
+    
 
 def EnemyDropsDesc():
     myDesc = PopupDescriptions.Description()
     myDesc.Header(Options.EnemyDropOption.name)
-    myDesc.Text("Randomizes the contents of chests dropped from enemies. You can choose the weights for the categories you have chosen.")
-    myDesc.Header(Options.QuestRewardsOption_KeepType.name)
-    myDesc.Text(keepTypeDescriptions)
+    myDesc.Text("Randomizes the contents of chests dropped from enemies. You can choose the weights for the categories you have chosen. Crystals are not randomized through this setting.")
     return myDesc
 
 
-# def QuestRewards():
-#     col = ItemType(IDs.CollectableIDs, Options.QuestRewardsOptions_Collectables)
-#     mat = ItemType(IDs.MaterialIDs, Options.QuestRewardsOptions_Materials)
-#     arm = ItemType(IDs.ArmorIDs, Options.QuestRewardsOptions_Armor)
-#     wep = ItemType(IDs.WeaponIDs, Options.QuestRewardsOptions_Weapons)
-#     gem = ItemType(IDs.GemIDs, Options.QuestRewardsOptions_Gems)
-#     cry = ItemType(IDs.CrystalIDs, Options.QuestRewardsOptions_Crystals)
-#     art = ItemType(IDs.ArtBookIDs, Options.QuestRewardsOptions_ArtBooks)
-#     key = ItemType(IDs.KeyItemIDs, Options.QuestRewardsOptions_KeyItems)
-#     keepType = Options.QuestRewardsOption_KeepType
+def QuestRewards():
+    valTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.MaterialIDs, Values.WeightOptionMethod(Options.QuestRewardsOptions_Materials))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.CollectableIDs, Values.WeightOptionMethod(Options.QuestRewardsOptions_Collectables))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.ArmorIDs, Values.WeightOptionMethod(Options.QuestRewardsOptions_Armor))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.WeaponIDs, Values.WeightOptionMethod(Options.QuestRewardsOptions_Weapons))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.GemIDs, Values.WeightOptionMethod(Options.QuestRewardsOptions_Gems))
+    valTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.ArtBookIDs, Values.WeightOptionMethod(Options.QuestRewardsOptions_ArtBooks))
     
-#     odds = Options.QuestRewardsOption.GetSpinbox()
-    
-#     areas = []
-#     for area in IDs.areaFileListNumbers:
-#         areas.append(f"bdat_common/JNL_quest{area}")
-    
-#     ItemRandomization([col, mat, arm, wep, gem, cry, art, key], areas, odds,"XCDE",  ["reward_A1","reward_A2","reward_A3","reward_B1","reward_B2","reward_B3"], keepType=keepType.GetState())
+    areas = []
+    rewardTypes = ["A1", "A2", "A3", "B1", "B2", "B3"]
+    for area in IDs.areaFileListNumbers:
+        areas.append(f"bdat_common/JNL_quest{area}")
+        
+    for area in areas:
+        with open(f"XCDE/JsonOutputs/{area}.json", 'r+', encoding='utf-8') as rewFile:
+            rewData = json.load(rewFile)
+            for rew in rewData["rows"]:
+                for type in rewardTypes:
+                    valTable.SelectValuedMember(rew, f"reward_{type}", IDs.KeyItemIDs)
+            JSONParser.CloseFile(rewData, rewFile)
 
-# def QuestRewardsDesc():
-#     myDesc = PopupDescriptions.Description()
-#     myDesc.Header(Options.QuestRewardsOption.name)
-#     myDesc.Text("Randomizes rewards from quests. You can choose the weights for the categories you have chosen.")
-#     myDesc.Header(Options.QuestRewardsOption_KeepType.name)
-#     myDesc.Text(keepTypeDescriptions)
-#     return myDesc
+def QuestRewardsDesc():
+    myDesc = PopupDescriptions.Description()
+    myDesc.Header(Options.QuestRewardsOption.name)
+    myDesc.Text("Randomizes rewards from quests. You can choose the weights for the categories you have chosen.")
+    return myDesc
 
 
-# def TradeOptions():
-#     col = ItemType(IDs.CollectableIDs, Options.TradeOptions_Collectables)
-#     mat = ItemType(IDs.MaterialIDs, Options.TradeOptions_Materials)
-#     arm = ItemType(IDs.ArmorIDs, Options.TradeOptions_Armor)
-#     wep = ItemType(IDs.WeaponIDs, Options.TradeOptions_Weapons)
-#     gem = ItemType(IDs.GemIDs, Options.TradeOptions_Gems)
-#     cry = ItemType(IDs.CrystalIDs, Options.TradeOptions_Crystals)
-#     art = ItemType(IDs.ArtBookIDs, Options.TradeOptions_ArtBooks)
-#     key = ItemType(IDs.KeyItemIDs, Options.TradeOptions_KeyItems)
-#     keepType = Options.TradeOption_KeepType
+def TradeOptions():
+    wpnValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    wpnValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.WeaponIDs)
+    headValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    headValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.HeadIDs)
+    leggValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    leggValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.LegIDs)
+    artValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    artValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.ArtBookIDs)
+    waistValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    waistValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.WaistIDs)
+    armValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    armValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.ArmIDs)
+    bodyValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    bodyValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.ChestIDs)
+    gemValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    gemValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.GemIDs)
+    colValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    colValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.CollectableIDs)
+    matValTable = Values.ValueTable(path = "XCDE/JsonOutputs/bdat_common")
+    matValTable.PopulateValues(Values.ValueFile("ITM_itemlist"), IDs.MaterialIDs)
     
-#     odds = Options.TradeOption.GetSpinbox()
     
-#     areas = []
-#     for area in IDs.areaFileListNumbers:
-#         areas.append(f"bdat_ma{area}/exchangelist{area}")
+    areas = []
+    for area in IDs.areaFileListNumbers:
+        areas.append(f"bdat_ma{area}/exchangelist{area}")
     
-#     ItemRandomization([col, mat, arm, wep, gem, cry, art, key], areas, odds,"XCDE",  ["wpn1", "head1", "body1", "arm1", "waist1", "legg1", "kessyou1", "kessyou2", "collect1", "collect2", "materia1", "materia2"], keepType=keepType.GetState())
+    for area in areas:
+        with open(f"XCDE/JsonOutputs/bdat_ma{area}/exchangelist{area}.json", 'r+', encoding='utf-8') as exchFile:
+            exchData = json.load(exchFile)
+            for exch in exchData["rows"]:
+                wpnValTable.SelectValuedMember(exch, "wpn1")
+                headValTable.SelectValuedMember(exch, "head1")
+                headValTable.SelectValuedMember(exch, "body1")
+                armValTable.SelectValuedMember(exch, "arm1")
+                waistValTable.SelectValuedMember(exch, "waist1")
+                leggValTable.SelectValuedMember(exch, "legg1")
+                for i in range(1,3):
+                    gemValTable.SelectValuedMember(exch, f"kessyou{i}")
+                    colValTable.SelectValuedMember(exch, f"collect{i}")
+                    matValTable.SelectValuedMember(exch, f"materia{i}")
+                
+            JSONParser.CloseFile(exchData, exchFile)
+    
 
-# def TradeOptionsDesc():
-#     myDesc = PopupDescriptions.Description()
-#     myDesc.Header(Options.TradeOption.name)
-#     myDesc.Text("Randomizes the trades NPCs make. Only the chosen suboptions will be randomized.\nThe categories will stay the same so helms will always replace helms and so on.")
-#     myDesc.Image("rondinecap.png", "XCDE", 800)
-#     myDesc.Header(Options.QuestRewardsOption_KeepType.name)
-#     myDesc.Text(keepTypeDescriptions)
-#     return myDesc
+def TradeOptionsDesc():
+    myDesc = PopupDescriptions.Description()
+    myDesc.Header(Options.TradeOption.name)
+    myDesc.Text("Randomizes the trades NPCs make. Only the chosen suboptions will be randomized.\nThe categories will stay the same so helms will always replace helms and so on.")
+    myDesc.Image("rondinecap.png", "XCDE", 800)
+    return myDesc
 
