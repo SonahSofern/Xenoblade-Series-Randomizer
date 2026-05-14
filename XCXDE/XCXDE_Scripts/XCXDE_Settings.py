8d04aad02c4876bfd4cc887d721dfc33195121a0
Ben <94713741+SonahSofern@users.noreply.github.com>
2026-05-14 11:25:52 -0900

    Update XCXDE_Settings.py

--- XCXDE/XCXDE_Scripts/XCXDE_Settings.py
+++ XCXDE/XCXDE_Scripts/XCXDE_Settings.py
@@ -1,6 +1,6 @@
 from XCXDE.XCXDE_Scripts import SeedNames, Options
 import scripts.XCRandomizer, scripts.JSONParser
-Version = "BETA"
+Version = "1.0.0"
 Game = "XCXDE"
 Title = "Xenoblade Chronicles X DE"
 backgrounds = ["flower.jpg", "sunset.jpg", "purple.jpg"]
