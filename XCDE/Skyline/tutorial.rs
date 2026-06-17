e4e3a08394e75b5ca38142e5f163056dd1897232
Ben <94713741+SonahSofern@users.noreply.github.com>
2026-06-17 11:57:40 -0900

    refactor: fixing file paths

--- XCDE/Skyline/tutorial.rs
+++ XCDE/Skyline/tutorial.rs
@@ -5,8 +5,8 @@ use std::mem::transmute;
 // XCDE
 
 extern "C" {
-    fn _ZN4game8DataUtil11setTutorialERKN2fw8DocumentEtbb(this: *mut c_void, p2: u16, p3: bool, p4: bool);
-    fn _ZN4game8DataUtil11getTutorialERKN2fw8DocumentEt(this: *mut c_void, p2: u16, p3: bool, p4: bool);
+    fn _ZN4game8DataUtil11setTutorialERKN2fw8DocumentEtbb(_p1: *mut c_void, p2: u16, p3: bool, p4: bool);
+    fn _ZN4game8DataUtil11getTutorialERKN2fw8DocumentEt(_p1: *mut c_void, p2: u16, p3: bool, p4: bool);
 }
 
 #[skyline::main(name = "XCDE")]
@@ -17,13 +17,13 @@ pub fn main() {
 }
 
 #[skyline::hook(replace = _ZN4game8DataUtil11getTutorialERKN2fw8DocumentEt)]
-fn skipTutorialHook(_this_ptr: *mut c_void, _short: c_ushort, _bool: bool) -> bool{
+fn skipTutorialHook(_ptr: *mut c_void, _short: c_ushort, _bool: bool) -> bool{
     unsafe {
-        if (!call_original!(_this_ptr, _short, _bool)){ // call getTutorial and if one exists then we call setTutorial immediately
+        if (!call_original!(_ptr, _short, _bool)){ // call getTutorial and if one exists then we call setTutorial immediately
             // Params true, false lead to the tutorial not being shown, gotten from void __thiscall game::DevGuiFlag::buildMpguiMessage(DevGuiFlag *this,MpguiContext *param_1)
             // This was a debug function that turned off tutorials for testing
             println!("[Randomizer] Skipped Tutorial ID (MNU_ttrl): {_short}");
-            _ZN4game8DataUtil11setTutorialERKN2fw8DocumentEtbb(_this_ptr, _short, true, false); 
+            _ZN4game8DataUtil11setTutorialERKN2fw8DocumentEtbb(_ptr, _short, true, false); 
         } 
     }
     true 
